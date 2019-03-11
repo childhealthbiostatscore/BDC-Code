781d1145e1dccde3bf992aece09217bb8c602109
Kristen Campbell <kristen.campbell@ucdenver.edu>
2019-03-11 14:17:39 -0600

    Update mixed_compare.R
    adding anova statement

--- Viral Shah/Gastroparesis/Code/mixed_compare.R
+++ Viral Shah/Gastroparesis/Code/mixed_compare.R
@@ -9,6 +9,7 @@ library(nlme)
 #time as a factor
 results_int.factor<-lme(average_sensor~factor(Period)*Group,random=~1|subject_id,data=dat.long)
 summary(results_int.factor)
+anova(results_int.factor)
 
 #time as continuous
 results_int.num<-lme(average_sensor~as.numeric(Period)*Group,random=~1|subject_id,data=dat.long)
