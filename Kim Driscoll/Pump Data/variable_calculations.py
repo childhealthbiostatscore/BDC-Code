a75462cbed0a1c5812149f9521e6d923265d7ac2
Tim Vigers <timothy.vigers@ucdenver.edu>
2019-05-11 15:58:06 -0600

    python code start

---
+++ Kim Driscoll/Pump Data/variable_calculations.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import os.path
+indir = "/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Kim Driscoll/Baseline Pump Paper/Data_Cleaned/Pump Files Cleaned"
+outdir = "/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Kim Driscoll/Baseline Pump Paper/Data_Cleaned"
+files = os.listdir(indir)
+print(files)
