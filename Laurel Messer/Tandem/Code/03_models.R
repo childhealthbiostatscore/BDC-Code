e37b37d8baffc513c18d6d42cb68f65c0492fa6c
Kristen Campbell <kristen.campbell@ucdenver.edu>
2019-08-19 14:44:30 -0600

    tandem models

--- Laurel Messer/Tandem/Code/03_models.R
+++ Laurel Messer/Tandem/Code/03_models.R
@@ -1,6 +1,7 @@
 ######MODELS FOR PRO TANDEM STUDY
 ###AUTHOR: KRISTEN CAMPBELL
 ###DATE: 8/15/2019
+require(lsmeans)
 
 library(nlme)
 library(lme4)
@@ -9,43 +10,95 @@ source('C:/Users/campbkri/Documents/GitHub/BDC-Code/Laurel Messer/Tandem/Code/01
 
 dat.temp<-dat[,c(1,6,30,116:117,142:144,227:232)]
 #might have to do this:
-# dat.temp$change1_factor1<-dat.temp$baseline_factor1-dat.temp$mid_factor1
-# dat.temp$change2_factor1<-dat.temp$mid_factor2-dat.temp$mid_factor1
+dat.temp$change1_factor1<-dat.temp$mid_factor1-dat.temp$baseline_factor1
+dat.temp$change2_factor1<-dat.temp$post6m_factor1-dat.temp$mid_factor1
 
-dat.long<-reshape(dat.temp, 
+dat.temp$change1_factor2<-dat.temp$mid_factor2-dat.temp$baseline_factor2
+dat.temp$change2_factor2<-dat.temp$post6m_factor2-dat.temp$mid_factor2
+
+hist(dat.temp$change1_factor1)
+hist(dat.temp$change2_factor1)
+hist(dat.temp$change1_factor2)
+hist(dat.temp$change2_factor2)
+
+
+#how many went up vs down:
+dat.temp$change1_up<-1
+dat.temp$change1_up[dat.temp$change1_factor1<0]<-0
+
+dat.temp$change2_factor1_up<-1
+dat.temp$change2_factor1_up[dat.temp$change2_factor1<0]<-0
+
+change1.factor1<-lm(dat.temp$change1_factor1~1)
+change1.factor1<-lm(dat.temp$change1_factor1~dat.temp$method_cat)
+change1.factor1<-lm(dat.temp$change1_factor1~dat.temp$method_cat+dat.temp$cgm_yn+dat.temp$Age
+                    +dat.temp$Gender+dat.temp$Baseline_A1C)
+
+summary(change1.factor1)
+plot(change1.factor1)
+ch1.fac1.means <- lsmeans(change1.factor1, c("method_cat"))
+
+
+change2.factor1<-lm(dat.temp$change2_factor1~1)
+change2.factor1<-lm(dat.temp$change2_factor1~dat.temp$method_cat+dat.temp$cgm_yn)
+change2.factor1<-lm(dat.temp$change2_factor1~dat.temp$method_cat+dat.temp$cgm_yn+dat.temp$Age
+                    +dat.temp$Gender+dat.temp$Baseline_A1C)
+
+summary(change2.factor1)
+plot(change2.factor1)
+ch2.fac1.means <- lsmeans(change2.factor1, c("method_cat"))
+
+####FACTOR 2 MODELS
+change1.factor2<-lm(dat.temp$change1_factor2~1)
+change1.factor2<-lm(dat.temp$change1_factor2~dat.temp$method_cat)
+change1.factor2<-lm(dat.temp$change1_factor2~dat.temp$method_cat+dat.temp$cgm_yn+dat.temp$Age
+                    +dat.temp$Gender+dat.temp$Baseline_A1C)
+
+summary(change1.factor2)
+plot(change1.factor2)
+ch1.fac2.means <- lsmeans(change1.factor2, c("method_cat"))
+
+
+change2.factor2<-lm(dat.temp$change2_factor2~1)
+change2.factor2<-lm(dat.temp$change2_factor2~dat.temp$method_cat+dat.temp$cgm_yn)
+change2.factor2<-lm(dat.temp$change2_factor2~dat.temp$method_cat+dat.temp$cgm_yn+dat.temp$Age
+                    +dat.temp$Gender+dat.temp$Baseline_A1C)
+
+summary(change2.factor2)
+plot(change2.factor2)
+ch2.fac2.means <- lsmeans(change2.factor2, c("method_cat"))
+
+
+###Mixed modeling: 
+
+dat.long<-reshape(dat.temp,
                   varying=c("baseline_factor1","baseline_factor2",
                             "mid_factor1","mid_factor2",
                             "post6m_factor1","post6m_factor2"),
                   v.names = c("factor1","factor2"),
-                  timevar = "time", 
-                  times = c("baseline", "mid", "post6m"), 
+                  timevar = "time",
+                  times = c("baseline", "mid", "post6m"),
         idvar="ExternalReference",direction="long")
 dat.long<-dat.long[order(dat.long$ExternalReference,dat.long$time),]
 
 dat.long$time<-factor(dat.long$time)
-
-#####FACTOR 1 - not normal
-require(MASS)
-qqp(dat.long$factor1, "norm")
-qqp(dat.long$factor1, "lnorm")
-gam <- fitdistr(dat.long$factor1, "Gamma")
-qqp(dat.long$factor1, "gamma", shape = gam$estimate[[1]], rate = gam$estimate[[2]])
-
-
-dat.long$factor1_2<-dat.long$factor1^3
-hist(dat.long$factor1_2)
-
-
-
-#LMM: can't do this
-factor1_uni<-lme(factor1~time+time*method_cat
-                 ,random=~1|ExternalReference,data=dat.long)
-plot(factor1_uni)
-summary(factor1_uni)
-
-
-#####FACTOR 2 - pretty normal
-factor2_uni<-lme(factor2~time+time*method_cat
-                     ,random=~1|ExternalReference,data=dat.long)
-plot(factor2_uni)
-summary(factor2_uni)
+# 
+# #####FACTOR 1 - not normal
+# 
+# dat.long$factor1_2<-dat.long$factor1^3
+# hist(dat.long$factor1_2)
+# 
+# 
+# 
+# #LMM: can't do this
+# factor1_uni<-lme(factor1~time+time*method_cat
+#                  ,random=~1|ExternalReference,data=dat.long)
+# plot(factor1_uni)
+# summary(factor1_uni)
+# 
+# 
+# #####FACTOR 2 - pretty normal
+# factor2_uni<-lme(factor2~time+time*method_cat
+#                      ,random=~1|ExternalReference,data=dat.long)
+# plot(factor2_uni)
+# summary(factor2_uni)
