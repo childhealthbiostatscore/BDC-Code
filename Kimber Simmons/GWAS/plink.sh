b8f50816e6ae4212b758f4db5522b884baa32206
Tim Vigers <timothy.vigers@cuanschutz.edu>
2019-09-17 13:27:29 -0600

    update paths

--- Kimber Simmons/GWAS/plink.sh
+++ Kimber Simmons/GWAS/plink.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Set directory
 cd /Volumes/som/PEDS/RI\ Biostatistics\ Core/Shared/Shared\ Projects/Laura/BDC/Projects/Kimber\ Simmons/GWAS/Data_Cleaned/
+# Combine files
+plink --noweb --bfile Simmons_redo --merge-list files_to_combine.txt --make-bed --out Simmons_combined
 # Raw file name
-file="Simmons_redo"
+file="Simmons_combined"
 # Check missing
 plink --noweb --bfile $file --missing
 # Delete SNPs and individuals with high levels of missingness
