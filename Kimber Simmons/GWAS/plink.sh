015ff884589da38f75b6aed86d75d304794ec7d7
Tim Vigers <timothy.vigers@cuanschutz.edu>
2019-09-23 15:35:58 -0600

    through MAF

--- Kimber Simmons/GWAS/plink.sh
+++ Kimber Simmons/GWAS/plink.sh
@@ -16,8 +16,15 @@ plink --noweb --bfile Simmons_combined_remove_miss --missing --out Simmons_combi
 plink --noweb --bfile Simmons_combined_remove_miss --check-sex
 # Remove subjects with problematic X chromosome homozygosity
 grep "PROBLEM" plink.sexcheck| awk '{print$1,$2}'> sex_discrepancy.txt
-plink --bfile Simmons_combined_remove_miss --remove sex_discrepancy.txt --make-bed --out Simmons_combined_remove_miss_and_sex --noweb
+plink --noweb --bfile Simmons_combined_remove_miss --remove sex_discrepancy.txt --make-bed --out Simmons_combined_remove_miss_and_sex --noweb
 # Check sex discrepancy again
-plink --noweb --bfile Simmons_combined_remove_miss --check-sex --out post_del
-# Minor allele frequency
-plink --noweb --bfile Simmons_combined_remove_miss --freq --out MAF_check
+plink --noweb --bfile Simmons_combined_remove_miss_and_sex --check-sex --out post_del
+# Select autosomal SNPs only (i.e., from chromosomes 1 to 22).
+awk '{ if ($1 >= 1 && $1 <= 22) print $2 }' Simmons_combined_remove_miss_and_sex > snp_1_22.txt
+plink --noweb --bfile Simmons_combined_remove_miss_and_sex --extract snp_1_22.txt --make-bed --out Simmons_combined_auto
+# Check minor allele frequency
+plink --noweb --bfile Simmons_combined_auto --freq --out MAF_check
+# Remove SNPs with a low MAF frequency.
+plink --noweb --bfile Simmons_combined_auto --maf 0.05 --make-bed --out Simmons_combined_auto_maf
+# Check minor allele frequency again
+plink --noweb --bfile Simmons_combined_auto_maf --freq --out MAF_check_post
