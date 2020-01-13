c0f761fd8b851f3b7408d0541c6645ecd3898816
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-01-13 12:02:10 -0700

    carbs

--- Kim Driscoll/Pump Data/pump_variables.py
+++ Kim Driscoll/Pump Data/pump_variables.py
@@ -1,15 +1,16 @@
 # Install packages if necessary - function to check
-import pip
-def import_or_install(package):
-    try:
-        __import__(package)
-    except ImportError:
-        pip.main(['install', package]) 
-# Required packages
-import_or_install("pandas")
+# import pip
+# def import_or_install(package):
+#     try:
+#         __import__(package)
+#     except ImportError:
+#         pip.main(['install', package]) 
+# # Required packages
+# import_or_install("pandas")
 # Load packages
 import pandas as pd
 import os
+import datetime as dt
 
 # List of files
 path = "/Users/timvigers/Desktop/Pump Files Original/"
@@ -25,14 +26,16 @@ data["Timestamp"] = data["Date"] + " " + data["Time"]
 all_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in data["Timestamp"]]
 data["Weekday"] = [dt.datetime.weekday(t) for t in all_times]
 # Total days of data
-days = (max(all_times) - min(all_times)).days 
-
+days = (max(all_times) - min(all_times)).days
 # Get BG reading times and convert to datetime
-bg_reading_times = all_times.loc[data["Sensor Calibration BG (mg/dL)"] > 0,"Timestamp"]
+bg_reading_times = data.loc[data["Sensor Calibration BG (mg/dL)"] > 0,"Timestamp"]
 bg_reading_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in bg_reading_times]
-# BG variables
+bg_reading_weekday = [dt.datetime.weekday(t) for t in bg_reading_times]
+# Basic BG variables
 total_readings = len(bg_reading_times)
 readings_per_day = total_readings / days
+readings_per_weekday = len([i for i in bg_reading_weekday if i <= 4]) / days
+readings_per_weekend = len([i for i in bg_reading_weekday if i >= 5]) / days
 # BG counters
 total_piu_70 = 0
 total_piu_70_149 = 0
@@ -64,3 +67,13 @@ for r in range(data.shape[0]):
             total_bwp_251_400 += 1
         elif bg > 400:
             total_bwp_above_400 += 1
+# Carbs
+# Get carb times and convert to datetime
+carb_times = data.loc[data["BWZ Carb Input (grams)"] > 0,"Timestamp"]
+carb_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in carb_times]
+carb_weekday = [dt.datetime.weekday(t) for t in carb_times]
+# Basic carb variables
+total_carbs = len(carb_times)
+carbs_per_day = total_carbs / days
+carbs_per_weekday = len([i for i in carb_weekday if i <= 4]) / days
+carbs_per_weekend = len([i for i in carb_weekday if i >= 5]) / days
