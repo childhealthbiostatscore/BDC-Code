a312588870aa9b6b3ea1881e788926dcdb2f6c08
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-01-13 12:52:44 -0700

    fixed bolus time check

--- Kim Driscoll/Pump Data/pump_variables.py
+++ Kim Driscoll/Pump Data/pump_variables.py
@@ -12,6 +12,7 @@ import pandas as pd
 import datetime as dt
 import os
 import collections
+import math
 
 # List of files
 path = "/Users/timvigers/Desktop/Pump Files Original/"
@@ -20,15 +21,21 @@ files = [path + f for f in files]
 
 # Read in data
 data = pd.read_csv(files[1])
-# Make Timestamp column, list of all times, weekday column
+# Sort
+data = data.sort_index()
+# Make timestamp column, sort by it 
 data["Timestamp"] = data["Date"] + " " + data["Time"]
 all_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in data["Timestamp"]]
+data["Timestamp"] = all_times
+data = data.sort_values(by = "Timestamp")
+# reset the index
+data.reset_index(inplace=True)
+# Weekday column
 data["Weekday"] = [dt.datetime.weekday(t) for t in all_times]
 # Total days of data
 days = (max(all_times) - min(all_times)).days
 # Get BG reading times and convert to datetime
 bg_reading_times = data.loc[data["Sensor Calibration BG (mg/dL)"] > 0,"Timestamp"]
-bg_reading_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in bg_reading_times]
 bg_reading_weekday = [dt.datetime.weekday(t) for t in bg_reading_times]
 # Basic BG variables
 total_readings = len(bg_reading_times)
@@ -68,7 +75,6 @@ for r in range(data.shape[0]):
             total_bwp_above_400 += 1
 # Get carb times and convert to datetime
 carb_times = data.loc[data["BWZ Carb Input (grams)"] > 0,"Timestamp"]
-carb_times = [dt.datetime.strptime(str(t),"%m/%d/%Y %H:%M:%S") for t in carb_times]
 carb_weekday = [dt.datetime.weekday(t) for t in carb_times]
 carb_dates = [dt.datetime.date(t) for t in carb_times]
 # Carb variables
@@ -78,6 +84,16 @@ carbs_per_weekday = len([i for i in carb_weekday if i <= 4]) / days
 carbs_per_weekend = len([i for i in carb_weekday if i >= 5]) / days
 carb_date_counts = collections.Counter(carb_dates)
 perc_days_3_more_carbs = len([i for i in carb_date_counts.values() if i >3]) / days
+# Boluses
+bolus_times = data.loc[data["Bolus Volume Delivered (U)"] > 0,"Timestamp"]
 # Iterate through rows - bolus as anchor
 for r in range(data.shape[0]):
+	# Get bolus value and time
+	bolus = data.loc[r,"Bolus Volume Delivered (U)"]
+	# If NaN, next
+	if math.isnan(bolus):
+		continue
+	bol_time = data.loc[r,"Timestamp"]
+	bol_period_end = bol_time + dt.timedelta(minutes=15)
+	# Check for additional boluses within 15 minutes
 	
