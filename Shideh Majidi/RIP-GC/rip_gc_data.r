8c6cb1249c532b3a55761f271b3186b1574d7fe3
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-02-03 16:19:39 -0700

    Update rip_gc_analysis.Rmd and rip_gc_data.r

--- Shideh Majidi/RIP-GC/rip_gc_data.r
+++ Shideh Majidi/RIP-GC/rip_gc_data.r
@@ -1,16 +1,30 @@
 library(tidyverse)
 # Read Data
 cgm_pump=read.csv('/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Shideh Majidi/RIPGC/Data_Cleaned/pump_cgm.csv',
-              na.strings = c("","."))
+              na.strings = c("",".","#N/A","N/A","*No day 2 - brother has T1D 10 yrs, well informed"))
 demographics=read.csv('/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Shideh Majidi/RIPGC/Data_Cleaned/demographics.csv',
                       na.strings = c("","."))
-# Find those on pump and CGM at any time
-device_use <- cgm_pump %>% group_by(ID) %>%
+a1cs=read.csv('/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Shideh Majidi/RIPGC/Data_Cleaned/a1cs.csv')
+# Summarise across all visits
+roc_data <- cgm_pump %>% group_by(ID) %>%
   summarise(cgm_use = suppressWarnings(max(OnCGM,na.rm = T)),
-            pump_use = suppressWarnings(max(OnPump,na.rm = T)))
-device_use[device_use == -Inf] <- NA
-# Add to demographics
-demographics <- left_join(demographics,device_use,by = "ID")
+            pump_use = suppressWarnings(max(OnPump,na.rm = T)),
+            bg_high = mean(BGHigh.,na.rm = T),
+            bg_ok = mean(BDOK.,na.rm = T),
+            bg_low = mean(BGLow.,na.rm = T),
+            cgm_high = mean(CGMHigh.,na.rm = T),
+            cgm_ok = mean(CGMOK.,na.rm = T),
+            cgm_low = mean(CGMLow.,na.rm = T),
+            ripgc = suppressWarnings(max(RIPGC_total_score,na.rm = T)))
+# Summarise HbA1c
+roc_data <- left_join(roc_data,
+                      a1cs %>% group_by(ID) %>% 
+                        summarise(a1c = mean(OrderValue,na.rm = T)),by = "ID")
+# NaN and -Inf to NA
+roc_data[is.na(roc_data)] <- NA
+roc_data[roc_data == -Inf] <- NA
+# RIPGC as factor
+roc_data$ripgc <- as.factor(roc_data$ripgc)
 # Factors
 demographics$Sex = factor(demographics$Sex,levels=c(1,0),
                           labels = c("Male","Female"))
@@ -32,14 +46,21 @@ demographics$Insurance = factor(demographics$Insurance,levels = c(0:2),
 demographics$RiskCategory_coded = factor(demographics$RiskCategory_coded,
                                          levels = c(0:2),
                                          labels = c("Low","Moderate","High"))
-demographics$cgm_use = factor(demographics$cgm_use,levels = c(0,1),
-                              labels = c("No","Yes"))
-demographics$pump_use = factor(demographics$pump_use,levels = c(0,1),
-                              labels = c("No","Yes"))
+
+roc_data$cgm_use = factor(roc_data$cgm_use,levels = c(0,1),
+                          labels = c("No","Yes"))
+
+roc_data$pump_use = factor(roc_data$pump_use,levels = c(0,1),
+                          labels = c("No","Yes"))
+
+demographics <- left_join(demographics,roc_data[,c("ID","cgm_use","pump_use")],
+                          by = "ID")
 
 # Other
 demographics$Initial_A1c <- 
   suppressWarnings(as.numeric(as.character(demographics$Initial_A1c)))
+
+cgm_pump$VisitDate <- lubridate::mdy(cgm_pump$VisitDate)
 
 # Labels
 labels(demographics) <- c(RaceEthnicity_combined="Race/Ethnicity",
@@ -48,7 +69,3 @@ labels(demographics) <- c(RaceEthnicity_combined="Race/Ethnicity",
                           Hospitalization="Hospitalized",
                           Endorsed_SI="Endorsed S.I.",
                           cgm_use="CGM Use",pump_use="Pump Use")
-
-
-
-
