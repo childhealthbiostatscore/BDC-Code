6c79b5577da33d53de368af0fbbdab7e07ebef0e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-04-02 17:30:28 -0600

    correct python modules

--- Laurel Messer/K23/power.py
+++ Laurel Messer/K23/power.py
@@ -1,10 +1,10 @@
-import statsmodels
+import statsmodels.stats.power
 import math
 import numpy as np
 # Normal power test
 normal = statsmodels.stats.power.NormalIndPower()
 n = normal.solve_power(effect_size=0.3, nobs1=None, alpha=0.05, power=0.8, ratio=1.0, alternative='two-sided')
-print
+print(math.ceil(n))
 # Example code
 # T-test instance
 power_analysis = statsmodels.stats.power.TTestIndPower()
@@ -12,4 +12,3 @@ power_analysis = statsmodels.stats.power.TTestIndPower()
 effect_sizes = np.array([0.2, 0.5, 0.8])
 sample_sizes = np.array(range(5, 100))
 power_analysis.plot_power(dep_var='nobs', nobs=sample_sizes, effect_size=effect_sizes)
-pyplot.show()(math.ceil(n) * 2)
