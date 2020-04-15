54dcbe27b62bbe12392a040a1149163e6a6a71a4
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-04-15 14:20:41 -0600

    Update k23_power.py

--- Laurel Messer/K23/k23_power.py
+++ Laurel Messer/K23/k23_power.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from tableone import TableOne
@@ -9,16 +10,18 @@ columns = ['70-180', 'HbA1c']
 t1 = TableOne(data, columns, decimals=2)
 print(t1)
 # Linear models
-# Drop missing
-data_no_miss = data.dropna()
-# Simplest
+# Drop missing rows
+data_no_miss = data[data['HbA1c'].notna() & data['70-180'].notna()]
+# Add gender dummy variable and intercept (HbA1c ~ b0 + b1(70-180) + b2(gender) + e)
 X = data_no_miss[['70-180']]
-Y = data_no_miss[['HbA1c']]
+dummy = pd.get_dummies(data_no_miss[['Patient gender']],drop_first=True)
+X = X.join(dummy)
+X = sm.add_constant(X)
+
+Y = np.array(data_no_miss[['HbA1c']])
 
 data_no_miss.plot(x='70-180', y='HbA1c', style='o')  
 
-# Add intercept to model
-X = sm.add_constant(X)
 # Regression
 model = sm.OLS(Y, X).fit()
 # Results
