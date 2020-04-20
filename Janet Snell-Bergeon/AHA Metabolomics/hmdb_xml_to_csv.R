af6b1c456f833bbdc8cf5158256d2ed2d0bec7f1
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-04-20 17:53:42 -0600

    Delete hmdb_xml_to_csv.R

--- Janet Snell-Bergeon/AHA Metabolomics/hmdb_xml_to_csv.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(xml2)
-hmdb <- read_xml('/Users/timvigers/Downloads/serum_metabolites.xml')%>%
-  as_list()
-
-id = unlist(sapply(hmdb, "[[", "accession"))
-name = unlist(sapply(hmdb, "[[", "name"))
-hmdbclass = sapply(hmdb, "[[", "class")
