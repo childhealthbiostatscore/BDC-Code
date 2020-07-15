d699eca15af498494ac745755f353862485978d2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-15 13:14:53 -0600

    reorganize files

---
+++ Kimber Simmons/GWAS/biobank/biobank_qc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /Users/timvigers/simmons/biobank
+# Check missing
+plink --bfile Simmons_070620 --missing
+# Delete SNPs and individuals with high levels of missingness
+# Delete SNPs
+plink --bfile Simmons_070620 --geno 0.02 --make-bed --out biobank_1
+# Delete individuals
+plink --bfile biobank_1 --mind 0.02 --make-bed --out biobank_1
+# Check missing post-deletion
+plink --bfile biobank_1 --missing --out miss_post_del
+# Check sex discrepancy
+plink --bfile biobank_1 --check-sex
