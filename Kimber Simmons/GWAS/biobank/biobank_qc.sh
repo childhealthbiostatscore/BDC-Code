2b09f61d791c6e95cc66c972b52b2af80e418d9f
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-16 15:01:21 -0600

    Update biobank_qc.sh

--- Kimber Simmons/GWAS/biobank/biobank_qc.sh
+++ Kimber Simmons/GWAS/biobank/biobank_qc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /Users/timvigers/simmons/biobank
 # Check missing
-plink --bfile Simmons_070620 --missing
+plink --bfile Simmons_071520 --missing
 # Delete SNPs and individuals with high levels of missingness
 # Delete SNPs
-plink --bfile Simmons_070620 --geno 0.02 --make-bed --out biobank_1
+plink --bfile Simmons_071520 --geno 0.02 --make-bed --out biobank_1
 # Delete individuals
 plink --bfile biobank_1 --mind 0.02 --make-bed --out biobank_1
 # Check missing post-deletion
