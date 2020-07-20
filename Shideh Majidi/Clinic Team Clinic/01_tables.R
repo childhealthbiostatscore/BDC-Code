8552761edad6a7f479735fdd16616726a6e448e3
Kristen Campbell <kristen.campbell@ucdenver.edu>
2020-07-20 12:49:08 -0600

    CTC

--- Shideh Majidi/Clinic Team Clinic/01_tables.R
+++ Shideh Majidi/Clinic Team Clinic/01_tables.R
@@ -4,7 +4,7 @@ source("S:/Shared Material/Shared Code/R/temp_table1.R")
 
 table1<-final_table(dat.one,c("Gender","Age_eachvisit","Race.Ethnicity","InsuranceType_VisitDate",
                               "CGM_Use","InsulinPump_Use","DurationOfDiabetes_eachvisit"),
-                        dat.one$group,margin=2,single=F,2,col.names=T, summary.stat='mean')
+                        dat.one$group,margin=2,single=F,2,col.names=T, summary.stat='median')
 table1
 
 table2<-final_table(dat.one,c("A1C_Value","Meter_BGLow","Meter_BGHigh","Meter_BGOK"), 
