dbbfbf50eb2d5d621133643611637dd7794a50aa
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-24 11:18:04 -0600

    Update biobank_qc.sh

--- Kimber Simmons/GWAS/biobank/biobank_qc.sh
+++ Kimber Simmons/GWAS/biobank/biobank_qc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cd /Users/timvigers/Documents/OneDrive\ -\ The\ University\ of\ Colorado\ Denver/simmons_biobank/
+cd /Users/timvigers/Documents/OneDrive\ -\ The\ University\ of\ Colorado\ Denver/simmons/
 # Make plink2 files, move to QC directory
-plink2 --bfile Simmons_071520 --make-pgen --out qc/simmons_qc
+plink2 --bfile Simmons_071520 --make-bed --out qc/simmons_biobank_qc
 cd qc
+# Check for sex discrepancies - per Nick, the 7 participants marked as problems are okay to keep in the study.
+plink --bfile simmons_biobank_qc --check-sex
 # Check missing
-plink2 --pfile simmons_qc --missing
+plink --bfile simmons_biobank_qc --missing
 # Delete SNPs and individuals with high levels of missingness
 # Delete SNPs
-plink --bfile simmons_qc --geno 0.02 --make-bed --out simmons_qc
+plink --bfile simmons_biobank_qc --geno 0.02 --make-bed --out simmons_biobank_qc
 # Delete individuals
-plink --bfile simmons_qc --mind 0.02 --make-bed --out simmons_qc
+plink --bfile simmons_biobank_qc --mind 0.02 --make-bed --out simmons_biobank_qc
 # Check missing post-deletion
-plink --bfile biobank_1 --missing --out miss_post_del
-# Check sex discrepancy
-plink --bfile biobank_1 --check-sex
+plink --bfile simmons_biobank_qc --missing --out miss_post_del
