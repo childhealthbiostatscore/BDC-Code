8c4e2296b3ece37949c38b41312bbb0dc06d0f09
Kristen Campbell <kristen.campbell@ucdenver.edu>
2020-08-05 07:38:33 -0600

    Update 03_analysis.R

--- Shideh Majidi/Clinic Team Clinic/03_analysis.R
+++ Shideh Majidi/Clinic Team Clinic/03_analysis.R
@@ -3,75 +3,59 @@ library(nlme)
 library(afex)
 require(lsmeans)
 
+#create categorical variables for analysis:
+dat.one$RTC_visits_cat_2<-NA
+dat.one$RTC_visits_cat_2[dat.one$RTC_visits_cat==2]<-"2-3"
+dat.one$RTC_visits_cat_2[dat.one$RTC_visits_cat==3]<-"2-3"
+dat.one$RTC_visits_cat_2[dat.one$RTC_visits_cat==4]<-"4+"
+dat.one$RTC_visits_cat_2[dat.one$RTC_visits_cat=="5+"]<-"4+"
+
+dat.one$Race.Ethnicity_2<-"White"
+dat.one$Race.Ethnicity_2[dat.one$Race.Ethnicity!="White"]<-"Non-White"
+
+dat.one$Race.Ethnicity_3<-"Other"
+dat.one$Race.Ethnicity_3[dat.one$Race.Ethnicity=="Hispanic"]<-"Hispanic"
+dat.one$Race.Ethnicity_3[dat.one$Race.Ethnicity=="White"]<-"White"
+
+dat.one$insurance_2<-"Private/Military"
+dat.one$insurance_2[dat.one$InsuranceType_VisitDate=="Public"]<-"Public"
+
 ###Aim 1a: Control group only
 ####SIMPLE: Change from pre to post as outcome:
 
 dat.c<-subset(dat.one,dat.one$group=="Control")
 
+
+# lm_1a<-lm(dat.c$avg_a1c_change~dat.c$RTC_visits_cat_2+dat.c$avg_a1c_pre)
+# summary(lm_1a)
+# plot(lm_1a)
+
+#avg_a1c_change: pre minus post (+ change means A1c went down, - change means A1c went up)
 lm_1a<-lm(dat.c$avg_a1c_change~dat.c$avg_a1c_pre+dat.c$RTC_visits_cat_2+dat.c$DurationOfDiabetes_eachvisit+
             dat.c$Gender+dat.c$insurance_2+dat.c$Race.Ethnicity_2)
 summary(lm_1a)
 ##plot(lm_1a)
 lm_1a_tab<-fx_model_linear('avg_a1c_change',c('avg_a1c_pre','RTC_visits_cat_2','DurationOfDiabetes_eachvisit',
                                      'Gender','insurance_2','Race.Ethnicity_2'),data=dat.c)
-lm_1a_tab$Variable<-c("Average A1c in RTC Period","4+ RTC Visits","Duration of Diabetes at First RTC","Female","Insurance - Public (vs. Private/Military)",
-"Race/Ethnicity - White (vs. others)")
-
-####LMM APPROACH:
-dat.c.all<-subset(dat,dat$group=="Control" & !is.na(dat$A1C_Value))
-
-lmm_1a<-lme(A1C_Value~days_from_first_visit+RTC_visits_cat_2+Gender+insurance_2+Race.Ethnicity_2+time_period
-           ,random=~1|MRN/days_from_first_visit,data=dat.c.all)
-lmm_1a_sum<-summary(lmm_1a)
-lmm_1a_sum<-lmm_1a_sum$tTable[,c(1,2,5)]
-lmm_1a_sum<-as.data.frame(lmm_1a_sum)
-lmm_1a_sum$Value<-round(lmm_1a_sum$Value,3)
-lmm_1a_sum$Std.Error<-round(lmm_1a_sum$Std.Error,3)
-lmm_1a_sum$`p-value`<-round(lmm_1a_sum$`p-value`,3)
-
+lm_1a_tab$Predictor_use<-
 ###Aim 1b: CTC group only
 ####SIMPLE: Change from pre to post as outcome:
 
 dat.ctc<-subset(dat.one,dat.one$group=="CTC")
+
+# lm_1b<-lm(dat.ctc$avg_a1c_post~dat.ctc$avg_a1c_pre+dat.ctc$total_RTC)
+# summary(lm_1b)
+# plot(lm_1b)
+
 lm_1b<-lm(dat.ctc$avg_a1c_change~dat.ctc$avg_a1c_pre+dat.ctc$RTC_visits_cat_2)
-lm_1b_tab<-fx_model_linear('avg_a1c_change',c('avg_a1c_pre','RTC_visits_cat_2'),data=dat.ctc)
-lm_1b_tab$Variable<-c("Average A1c in RTC Period","4+ RTC Visits")
-
-####LMM APPROACH:
-dat.ctc.all<-subset(dat,dat$group=="CTC"  & !is.na(dat$A1C_Value))
-
-lmm_1b<-lme(A1C_Value~days_from_first_visit+RTC_visits_cat_2+time_period
-            ,random=~1|MRN/days_from_first_visit,data=dat.c.all)
-lmm_1b_sum<-summary(lmm_1b)
-lmm_1b_sum<-lmm_1b_sum$tTable[,c(1,2,5)]
-lmm_1b_sum<-as.data.frame(lmm_1b_sum)
-lmm_1b_sum$Value<-round(lmm_1b_sum$Value,3)
-lmm_1b_sum$Std.Error<-round(lmm_1b_sum$Std.Error,3)
-lmm_1b_sum$`p-value`<-round(lmm_1b_sum$`p-value`,3)
+summary(lm_1b)
+#plot(lm_1b)
 
 ####Aim 2: CTC versus Control group: 
 lm_2<-lm(dat.one$avg_a1c_change~dat.one$avg_a1c_pre+dat.one$RTC_visits_cat_2+dat.one$group
          #+dat.one$DurationOfDiabetes_eachvisit+dat.one$Gender+dat.one$insurance_2+dat.one$Race.Ethnicity_2+dat.one$group
             )
-summary(lm_2)
-
-lm_2_tab<-fx_model_linear('avg_a1c_change',c('avg_a1c_pre','RTC_visits_cat_2','group'),data=dat.one)
-lm_2_tab$Variable<-c("Average A1c in RTC Period","4+ RTC Visits","CTC Group")
-
-#Linear mixed model of slope of A1c in post period, by group:
-dat.post<-subset(dat,dat$time_period!="During RTC")
-
-dat.post<-subset(dat.post,!is.na(dat.post$A1C_Value))
-
-lmm_2<-lme(A1C_Value~days_from_first_visit+RTC_visits_cat_2+group
-                ,random=~1|MRN/days_from_first_visit,data=dat.post)
-lmm_2_sum<-summary(lmm_2)
-lmm_2_sum<-lmm_2_sum$tTable[,c(1,2,5)]
-lmm_2_sum<-as.data.frame(lmm_2_sum)
-lmm_2_sum$Value<-round(lmm_2_sum$Value,3)
-lmm_2_sum$Std.Error<-round(lmm_2_sum$Std.Error,3)
-lmm_2_sum$`p-value`<-round(lmm_2_sum$`p-value`,3)
-
+#summary(lm_2)
 
 #####set up for mixed model with changepoint:
 changepoint<-function(ID,data){
