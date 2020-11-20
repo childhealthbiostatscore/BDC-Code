3587cf581382d0e2046001cbf8e664e07965176e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-11-20 12:41:48 -0700

    Create clean_tidepool_files.R

---
+++ Kim Driscoll/Pump Data/clean_tidepool_files.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(tools)
+# Original files
+indir = "/Users/timvigers/Desktop/Tidepool Files"
+outdir = "/Users/timvigers/Desktop/tidepool_test"
+files = list.files(indir,full.names = T)
+# Iterate through
+for (f in files) {
+  id = file_path_sans_ext(basename(f))
+  # Read tab names
+  tabs = excel_sheets(f)
+  # Boluses
+  if (all(!grepl("bolus",tolower(tabs)))){next}
+  bolus = read_excel(f,"Bolus")
+  bolus = bolus %>% select(`Local Time`,`Sub Type`,Extended,Normal)
+  bolus$Normal[!is.na(bolus$Extended)] = bolus$Normal[!is.na(bolus$Extended)] +
+    bolus$Extended[!is.na(bolus$Extended)]
+  bolus$Extended = NULL
+  colnames(bolus) = c("datetime","Bolus Type","Bolus Volume Delivered (U)")
+  bolus = as.data.frame(bolus)
+  # BG checks
+  smbg = read_excel(f,"SMBG")
+  smbg = smbg %>% select(`Local Time`,Value)
+  colnames(smbg) = c("datetime","bg")
+  smbg = as.data.frame(smbg)
+  # Carbs and bolus recommendation if available
+  if ("Bolus Calculator" %in% tabs) {
+    carbs = read_excel(f,"Bolus Calculator")
+    carbs = carbs %>% select(`Local Time`,`Carb Input`,`Recommended Correction`)
+  } else {
+    carbs = matrix(nrow = 1,ncol = 3)
+  }
+  colnames(carbs) = c("datetime","BWZ Carb Input (grams)","BWZ Estimate (U)")
+  carbs = as.data.frame(carbs)
+  # Merge
+  t = full_join(smbg,bolus,by = "datetime")
+  t = full_join(t,carbs,by = "datetime")
+  t$Date = lubridate::date(t$datetime)
+  t$Time = sub(".* ","",t$datetime)
+  t$datetime = NULL
+  # Write
+  write.csv(t,file = paste0(outdir,"/",id,".csv"),row.names = F,na = "")
+}
