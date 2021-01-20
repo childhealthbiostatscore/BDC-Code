b515e8f3e95172d9355940f758207a66b1d0539a
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-01-19 19:30:46 -0700

    genomic data merged!

--- Kimber Simmons/GWAS/check_variants.R
+++ Kimber Simmons/GWAS/check_variants.R
@@ -1,32 +1,19 @@
 setwd("/home/tim/Work/Kimber Simmons/GWAS/Data_Cleaned/plink")
 miss_snp = read.table("merged2-merge.missnp")
-# Import and sort
-redo_flip = read.table("./redo_flip.bim")
-redo_flip = redo_flip[redo_flip$V2 %in% miss_snp$V1,]
-redo_flip = redo_flip[order(redo_flip$V2),]
-
+# Import
 redo = read.table("./redo.bim")
 redo = redo[redo$V2 %in% miss_snp$V1,]
-redo = redo[order(redo$V2),]
 
 biobank = read.table("./biobank1.bim")
 biobank = biobank[biobank$V2 %in% miss_snp$V1,]
-biobank = biobank[order(biobank$V2),]
 
 # Exclude SNPs where alleles are different
 exclude_snps = unique(redo$V2[redo$V5 != "."])
-write.table(exclude_snps,file = "exclude_snps.txt",row.names = F,quote = F,col.names = F)
+write.table(exclude_snps,file = "exclude_snps",row.names = F,quote = F,col.names = F)
 
-# Find which are correct but have an allele missing in the redo file
-redo_correct = which(redo$V6 == biobank$V6)
-redo$V5[redo_correct] = biobank$V5[redo_correct]
+# Allele update file
+redo[,c(1,3,4)] = NULL
+redo = cbind(redo,biobank[match(redo$V2,biobank$V2),c(5,6)])
 
-flip_correct = which(redo_flip$V6 == biobank$V6)
-redo$V5[flip_correct] = biobank$V5[flip_correct]
-redo$V6[flip_correct] = redo_flip$V6[flip_correct]
+write.table(redo,file = "redo_update",row.names = F,quote = F,col.names = F)
 
-# Write
-redo_all = read.table("./redo.bim")
-redo_all$V5[match(redo$V2,redo_all$V2)] = redo$V5
-redo_all$V6[match(redo$V2,redo_all$V2)] = redo$V6
-write.table(redo_all,file = "redo_final.bim",row.names = F,quote = F,col.names = F)
--- Kimber Simmons/GWAS/final_analysis_plink.sh
+++ Kimber Simmons/GWAS/final_analysis_plink.sh
@@ -9,12 +9,18 @@ plink2 --bfile Data_Raw/V2\ -\ Biobank\ data\ on\ Hispanic\ Patients\ -\ Full\ G
 Rscript /home/tim/Documents/GitHub/BDC-Code/Kimber\ Simmons/GWAS/check_samples.R
 # Move to cleaned Data_Raw
 cd Data_Cleaned/plink
-# Try to merge
+# Try to merge - lots of flipped SNPs
 plink --bfile redo --bmerge biobank1 --make-bed --out merged1
 plink --bfile redo --flip merged1-merge.missnp --make-bed --out redo_flip
 plink --bfile redo_flip --bmerge biobank1 --make-bed --out merged2
-# Check alleles
-
-
-
-plink --bfile merged1 --bmerge biobank2 --make-bed --out merged2
+# R code to update alleles
+Rscript /home/tim/Documents/GitHub/BDC-Code/Kimber\ Simmons/GWAS/check_variants.R
+# Plink update alleles
+plink2 --bfile redo --update-alleles redo_update --make-bed --out redo_update
+# Try merge again
+plink --bfile redo_update --bmerge biobank1 --make-bed --out merged1
+plink --bfile redo_update --flip merged1-merge.missnp --make-bed --out redo_flip_update
+plink --bfile redo_flip_update --bmerge biobank1 --make-bed --out merge_post_flip
+# Remove duplicates
+plink --bfile merge_post_flip --list-duplicate-vars suppress-first
+plink2 --bfile merge_post_flip --exclude plink.dupvar --make-bed --out merged_final
