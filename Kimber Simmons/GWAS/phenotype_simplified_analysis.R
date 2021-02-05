7ef9eb45c58c9d15e1427d60630ea8037299e6dd
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-05 10:26:59 -0700

    rename files

--- Kimber Simmons/GWAS/phenotype_simplified_analysis.R
+++ Kimber Simmons/GWAS/phenotype_simplified_analysis.R
@@ -1,9 +1,9 @@
 setwd("/mnt/c/Users/Tim\ Vigers/Dropbox/Work/Kimber\ Simmons/GWAS/")
 # Read in and re-write .fam files
-redo = read.table("./Data_Cleaned/analysis_no_biobank/redo.fam")
+redo = read.table("./Data_Cleaned/simplified_analysis/redo.fam")
 clinical = read.csv("./Data_Cleaned/clinical.csv")
 clinical$Hispanic..0.N..1.Y. = 
   factor(clinical$Hispanic..0.N..1.Y.,labels = c("Non-Hispanic","Hispanic"))
 
 redo$V6 = as.numeric(clinical$Hispanic..0.N..1.Y.[match(gsub(".*_","",redo$V2),clinical$correct_labID)])
-write.table(redo,file = "./Data_Cleaned/analysis_no_biobank/redo.fam",row.names = F,quote = F,col.names = F)
+write.table(redo,file = "./Data_Cleaned/simplified_analysis/redo.fam",row.names = F,quote = F,col.names = F)
