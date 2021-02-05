3f24c8fbdece106404e5936839f3eefe03759de2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-05 15:29:58 -0700

    Update simplified_analysis.sh

--- Kimber Simmons/GWAS/simplified_analysis.sh
+++ Kimber Simmons/GWAS/simplified_analysis.sh
@@ -15,28 +15,38 @@ plink --bfile biobank1 --bmerge biobank2 --make-bed --out biobank_merged
 plink --bfile biobank_merged --list-duplicate-vars suppress-first
 plink2 --bfile biobank_merged --exclude plink.dupvar --make-bed --out biobank_merged
 # QC - keeping the datasets separate for this analysis
-# Check sex
-plink --bfile redo --check-sex
-plink --bfile biobank_merged --check-sex
-# Check missing
-plink2 --bfile redo --missing
+# Check sex and exclude problems
+plink --bfile redo --check-sex --out redo
+grep "PROBLEM" redo.sexcheck | awk '{print$1,$2}'> sex_discrepancy_redo.txt
+plink --bfile redo --remove sex_discrepancy_redo.txt --make-bed --out redo
+plink --bfile biobank_merged --check-sex --out biobank
+grep "PROBLEM" biobank.sexcheck | awk '{print$1,$2}'> sex_discrepancy_biobank.txt
+plink --bfile biobank_merged --remove sex_discrepancy_biobank.txt --make-bed --out biobank_merged
 # Delete SNPs and individuals with high levels of missingness
 # Delete SNPs
-plink2 --bfile redo --geno 0.02 --make-bed --out redo
+plink --bfile redo --geno 0.02 --make-bed --out redo
+plink --bfile biobank_merged --geno 0.02 --make-bed --out biobank_merged
 # Delete individuals
-plink2 --bfile redo --mind 0.02 --make-bed --out redo
-# Check missing post-deletion
-plink2 --bfile redo --missing --out miss_post_del
+plink --bfile redo --mind 0.02 --make-bed --out redo
+plink --bfile biobank_merged --mind 0.02 --make-bed --out biobank_merged
 # Remove variants based on MAF.
-plink2 --bfile redo --maf 0.05 --make-bed --out redo
+plink --bfile redo --maf 0.05 --make-bed --out redo
+plink --bfile biobank_merged --maf 0.05 --make-bed --out biobank_merged
 # Hardy-Weinberg equilibrium
-plink2 --bfile redo --hwe 1e-10 --make-bed --out redo
+plink --bfile redo --hwe 1e-10 --make-bed --out redo
+plink --bfile biobank_merged --hwe 1e-10 --make-bed --out biobank_merged
 # Check kinship - duplicate samples have kinship 0.5, not 1. none at 0.354 level
-plink2 --bfile redo --make-king-table
+plink2 --bfile redo --king-cutoff 0.354 --out redo
+plink2 --bfile biobank_merged --king-cutoff 0.354 --out biobank_merged
 # Remove temporary files
-rm redo.bed~ redo.bim~ redo.fam~
+find . -type f -name "*~" -delete
+# Impute alleles
+#plink --bfile redo --recode vcf
+#minimac4 --refHaps /mnt/c/Users/Tim\ Vigers/Dropbox/Work/TGP/G1K_P3_M3VCF_FILES_WITH_ESTIMATES.tar.gz --haps redo.vcf --prefix testRun
 # Analysis
 # Allele frequency
-plink --bfile redo --freq 'case-control' --out redo_freq
+plink --bfile redo --freq 'case-control' --out redo
+plink --bfile biobank_merged --freq 'case-control' --out biobank_merged
 # Logistic regression with ethnicity as outcome
-plink --bfile redo --logistic sex --adjust --out logistic_results
+plink --bfile redo --logistic sex --adjust --out redo
+plink --bfile biobank_merged --logistic sex --adjust --out biobank_merged
