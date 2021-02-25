a8f24c5fa4f4ba8ff46b2a4bdc9558da62786c36
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-25 07:18:35 -0700

    fixed some name matching issues for Sarit

--- Sarit Polsky/Pregnancy/clean_files.R
+++ Sarit Polsky/Pregnancy/clean_files.R
@@ -1,10 +1,11 @@
 library(lubridate)
-setwd("/Volumes/som/BDC/SHARED/POLSKY/Triple C/Tim/")
+library(cgmanalysis)
+setwd("/Users/timvigers/Dropbox/Work/Sarit Polsky/Triple C/Tim/")
 dateparseorder <- c("mdy","mdy HM","mdy HMS","mdY HM","mdY HMS","dmy HM","dmy HMS",
                     "dmY HM","dmY HMS","Ymd HM","Ymd HMS","ymd HM","ymd HMS",
                     "Ydm HM","Ydm HMS","ydm HM","ydm HMS")
 # Output location
-outdir = "/Volumes/som/BDC/SHARED/POLSKY/Triple C/Tim/"
+outdir = paste0(getwd(),"/","Cleaned/")
 # Import dates 
 dates = read.csv("./Trimester Dates -Janet.csv",na.strings = "")
 # List all the directories
@@ -13,11 +14,11 @@ dirs = list.dirs("RAW DATA- CGM downloads")
 for (d in dirs[2:length(dirs)]) {
   # Get name
   name = tolower(sub("_.*","",basename(d)))
-  name = sub(" ","",name)
+  name = gsub(" ","",name)
   last_name = strsplit(name,",")[[1]][1]
   first_name = strsplit(name,",")[[1]][2]
   # Get dates
-  r = which(tolower(dates$Last.name) == last_name & tolower(dates$First.name) ==  first_name)
+  r = which(tolower(gsub(" ","",dates$Last.name)) == last_name & tolower(dates$First.name) ==  first_name)
   t0 = parse_date_time(dates$t.0[r],dateparseorder,tz = "UTC")
   wk14 = parse_date_time(dates$X14.wks[r],dateparseorder,tz = "UTC")
   wk28 = parse_date_time(dates$X28.wks[r],dateparseorder,tz = "UTC")
@@ -26,7 +27,6 @@ for (d in dirs[2:length(dirs)]) {
   files = list.files(d,full.names = T)
   # Loop through files, combine into 1
   l = lapply(files, function(f) {
-    print(f)
     df = read.csv(f,na.strings = "",header = F)
     # Check file type by number of columns
     if (ncol(df) == 14) {
@@ -65,23 +65,26 @@ for (d in dirs[2:length(dirs)]) {
   df = df[order(df$timestamp),]
   # Split and write CSVs
   t0_wk14 = df[df$timestamp >= t0 & df$timestamp < wk14,]
-  if (nrow(t0_wk14)>0){
+  if (nrow(t0_wk14)>0 & sum(is.na(t0_wk14$sensorglucose))<nrow(t0_wk14)){
     t0_wk14$subjectid[1] = id
     write.csv(t0_wk14,file = paste0(outdir,last_name,"_",first_name,"_t0_wk14.csv"),
               row.names = F,na = "")
   }
   
   wk14_wk28 = df[df$timestamp >= wk14 & df$timestamp < wk28,]
-  if (nrow(wk14_wk28) > 0) {
+  if (nrow(wk14_wk28)>0 & sum(is.na(wk14_wk28$sensorglucose))<nrow(wk14_wk28)) {
     wk14_wk28$subjectid[1] = id
     write.csv(wk14_wk28,file = paste0(outdir,last_name,"_",first_name,"_wk14_wk28.csv"),
               row.names = F,na = "")
   }
   
   wk28_dd = df[df$timestamp >= wk28 & df$timestamp < dd,]
-  if(nrow(wk28_dd) > 0) {
+  if(nrow(wk28_dd)>0 & sum(is.na(wk28_dd$sensorglucose))<nrow(wk28_dd)) {
     wk28_dd$subjectid[1] = id
     write.csv(wk28_dd,file = paste0(outdir,last_name,"_",first_name,"_wk28_dd.csv"),
               row.names = F,na = "")
   }
 }
+# Variables
+cgmvariables("./Cleaned","./Reports",id_filename = T,
+             outputname = paste("polsky_triple_c_cgm",Sys.Date()))
