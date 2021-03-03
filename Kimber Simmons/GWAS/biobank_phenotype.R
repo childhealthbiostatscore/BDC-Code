1969ea7892e4502bcf46fc9a4282887fe1d17bcb
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-03-03 08:15:12 -0700

    plink 1.9 for all but kinship test

--- Kimber Simmons/GWAS/biobank_phenotype.R
+++ Kimber Simmons/GWAS/biobank_phenotype.R
@@ -1,11 +1,11 @@
-setwd("/media/tim/Work/Kimber Simmons/GWAS")
+setwd("/Users/timvigers/Dropbox/Work/Kimber Simmons/GWAS")
 # Read in and re-write .fam files
-redo = read.table("./Data_Cleaned/plink/redo.fam")
+redo = read.table("./Data_Cleaned/biobank_analysis/redo.fam")
 redo$V6 = 2
-write.table(redo,file = "./Data_Cleaned/plink/redo.fam",row.names = F,quote = F,col.names = F)
+write.table(redo,file = "./Data_Cleaned/biobank_analysis/redo.fam",row.names = F,quote = F,col.names = F)
 
-biobank = read.table("./Data_Cleaned/plink/biobank2.fam")
+biobank = read.table("./Data_Cleaned/biobank_analysis/biobank2.fam")
 diagnoses = read.csv("./Data_Raw/V2 - Biobank data on Hispanic Patients - Full Genetic Request/Table 2 Diagnoses.CSV")
 biobank$V6 = 2
 biobank$V6[which(!(biobank$V1 %in% diagnoses$Arb_Person_ID))] = 1
-write.table(biobank,file = "./Data_Cleaned/plink/biobank2.fam",row.names = F,quote = F,col.names = F)
+write.table(biobank,file = "./Data_Cleaned/biobank_analysis/biobank2.fam",row.names = F,quote = F,col.names = F)
