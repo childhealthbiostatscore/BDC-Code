4053341549ffc676118bb101ce53175c659d15ee
laurapyle <laurapyle@gmail.com>
2021-03-02 19:49:07 -0700

    Adding LASSO

--- Janet Snell-Bergeon/AHA combined model/clinical model.R
+++ Janet Snell-Bergeon/AHA combined model/clinical model.R
@@ -1,4 +1,6 @@
 library(sas7bdat)
+library(glmnet)
+library(tidyr)
 
 allvisits_long <- read.sas7bdat("Y:\\CACTI\\Analysis of Data\\SAS Datasets\\Core Datasets\\Longitudinal\\allvisits_long.sas7bdat")
 v1 <- allvisits_long[allvisits_long$Visit==1,]
@@ -19,8 +21,35 @@ final <- merge(final,ua,by="StudyID",all.x = T, all.y = F)
 pat <- read.sas7bdat("Y:\\CACTI\\Analysis of Data\\SAS Datasets\\PAT V1-V3\\PATwide.sas7bdat")
 pat$StudyID <- pat$STUDYID
 pat <- pat[,c("StudyID","pfatcm_v1")]
-final <- merge(final,ua,by="StudyID",all.x = T, all.y = F)
+final <- merge(final,pat,by="StudyID",all.x = T, all.y = F)
 final <- final[,-1]
 final <- final[final$CACprogV3 != "Unknown",]
 
+final$CACprogV3_num <- ifelse(final$CACprogV3=="Progression",1,0)
+final$CACprogV3 <- NULL
+final$alb_num <- as.factor(ifelse(final$albuminuria %in% c("mac","mic"),1,
+                        ifelse(final$albuminuria=="non",0,NA)))
+final$albuminuria <- NULL
+final$smknum <- as.factor(ifelse(final$smkstatus=="Current",1,
+                       ifelse(final$smkstatus %in% c("Former","Never"),0,NA)))
+final$smkstatus <- NULL
+final$onhypermeds <- as.factor(final$onhypermeds)
+final$onlipidmeds <- as.factor(final$onlipidmeds)
+final$onstatinmeds <- as.factor(final$onstatinmeds)
+
 write.csv(final,"B:\\Projects\\Janet Snell-Bergeon\\AHA collaborative grant\\Combined predictive model\\clinical data for lasso.csv",row.names = F)
+
+# only complete cases
+complete <- drop_na(final)
+
+# LASSO works but CV doesn't
+# it used to work until I made some of the variables factors
+x <- as.matrix(subset(complete,select=-CACprogV3_num))
+glmmod1 <- glmnet(x, y=as.factor(complete$CACprogV3_num), alpha=1, family="binomial")
+
+cv.glmmod <- cv.glmnet(x, y=complete$CACprogV3_num, alpha=1)
+best.lambda <- cv.glmmod$lambda.min
+glmmod1$lambda==best.lambda
+test <- glmmod1$lambda-best.lambda
+
+round(coef(glmmod1,s=best.lambda),4)
