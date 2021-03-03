a45c7b28d37dbeb6b40912daa720d75c40a01230
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-03-03 11:37:32 -0700

    better vcf filtering for imputation

--- Kimber Simmons/GWAS/biobank_analysis.sh
+++ Kimber Simmons/GWAS/biobank_analysis.sh
@@ -53,13 +53,21 @@ find . -name "*~" -delete
 plink --bfile merged_final --recode vcf --out merged_final
 # Compress
 bgzip -c merged_final.vcf > merged_final.vcf.gz
-# Loop tabix over all chromosomes to split into individual files
+# Loop tabix and bcftools over all chromosomes to split into individual files
+# Code from https://www.biostars.org/p/173073/ and
+# https://bioinformatics.stackexchange.com/questions/3401/how-to-subset-a-vcf-by-chromosome-and-keep-the-header
+# Autosomes are coded 1 - 22 and the others are:
+# X chromosome                    = 23
+# Y chromosome                    = 24
+# Pseudo-autosomal region of X    = 25
+# Mitochondrial                   = 26
 tabix -p vcf merged_final.vcf.gz
-tabix -h merged_final.vcf.gz chr1 > chr/chr1.vcf
-# Split into individual chromosomes
-bcftools view -r 1 merged_final.vcf
-# Minimac imputation
-minimac4 --refHaps /Users/timvigers/Dropbox/Work/GWAS/Minimac/G1K_P3_M3VCF_FILES_WITH_ESTIMATES/1.1000g.Phase3.v5.With.Parameter.Estimates.m3vcf.gz \
-         --haps chr/chr1.vcf \
+for i in {1..26}
+do
+   bcftools filter merged_final.vcf.gz -r $i > chr/chr$i.vcf
+done
+# Minimac imputation for autosomes
+minimac4 --refHaps /Users/timvigers/Dropbox/Work/GWAS/Minimac/G1K_P3_M3VCF_FILES_WITH_ESTIMATES/X.Non.Pseudo.Auto.1000g.Phase3.v5.With.Parameter.Estimates.m3vcf.gz \
+         --haps chr/chr23.vcf \
          --prefix testRun \
          --cpus 4
