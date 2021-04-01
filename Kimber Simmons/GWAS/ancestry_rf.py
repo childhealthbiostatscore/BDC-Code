1a8db95bc9afbc733879d98a3dd703e581e10683
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-04-01 11:15:36 -0600

    much better RF accuracy

--- Kimber Simmons/GWAS/ancestry_rf.py
+++ Kimber Simmons/GWAS/ancestry_rf.py
@@ -1,3 +1,4 @@
+import scipy
 import sklearn
 import pandas as pd
 # Import data
@@ -6,15 +7,18 @@ sample_info = pd.read_csv(wd+"super_pop.csv")
 pca = pd.read_csv(wd+"plink.eigenvec", sep=' ')
 # Join
 data = pca.set_index('IID').join(sample_info.set_index('IID'))
-# Get outcome (Y) and data (X)
+# Get outcome (Y) and data (X) - first 6 PCs
 y = data[["SuperPop"]]
-x = data[["PC" + s for s in ["1","2","3","4","5","6"]]]
+x = data[["PC"+str(n) for n in range(1, 6+1)]]
+from sklearn.ensemble import RandomForestClassifier
 # Set up a classifier with 100 bootstrap samples
-clf = sklearn.ensemble.RandomForestClassifier(bootstrap=True,max_samples=100)
+clf = RandomForestClassifier(bootstrap=True,max_samples=100)
 # Fit
 model = clf.fit(X = x,y = y.values.ravel())
+from sklearn.model_selection import train_test_split
 # Test the model
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.70)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.75,random_state=1017)
 model = clf.fit(X_train,y_train.values.ravel())
 y_pred=clf.predict(X_test)
-print("Accuracy:",sklearn.metrics.accuracy_score(y_test, y_pred))
+print("Accuracy of RF model based on a 75/25 training/test split of 1k Genomes data:",\
+    sklearn.metrics.accuracy_score(y_test, y_pred).round(3))
