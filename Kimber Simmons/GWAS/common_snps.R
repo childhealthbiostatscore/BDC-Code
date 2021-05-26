512d102820aa2c1aa31881df886e9b878faede93
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-05-25 18:53:11 -0600

    delete unnecessary GWAS code

--- Kimber Simmons/GWAS/common_snps.R
+++
@@ -1,5 +0,0 @@
-setwd("/home/tim/Work/Kimber Simmons/GWAS/Data_Cleaned/plink")
-
-kimber = read.delim("./merged_final.bim",header=F, quote="")
-tgp = read.delim("./all_phase3.bim", header=F, quote="")
-kimber = kimber[kimber$V2 %in% tgp$V2,]
