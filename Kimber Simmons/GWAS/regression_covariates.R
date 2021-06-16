00176e3a29aae795ba72d08b3ebbd6ca8a4127a6
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-06-16 10:29:37 -0600

    Update regression_covariates.R

--- Kimber Simmons/GWAS/regression_covariates.R
+++ Kimber Simmons/GWAS/regression_covariates.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(caret)
 setwd("~/Dropbox/Work/Kimber Simmons/GWAS/Data_Cleaned/biobank_analysis/imputed/")
 pc_num = 4
 # Read in and process the covariates
@@ -9,3 +10,9 @@ colnames(pcs) = c("FID","IID", paste0("PC",1:(ncol(pcs)-2)))
 covariate$Pheno = NULL
 covariate = merge(covariate,pcs)
 write.table(covariate,"./covar.txt",quote = F,sep = "\t",row.names = F)
+# Test - train split for evaluation
+set.seed(1017)
+pheno = read.table("./merged_imputed_qc.fam")
+train_index = createDataPartition(pheno$V6,p=0.75,list=F)
+write.table(pheno[train_index,1:2],"./train_samples",quote = F,sep = "\t",row.names = F,col.names = F)
+write.table(pheno[-train_index,1:2],"./test_samples",quote = F,sep = "\t",row.names = F,col.names = F)
