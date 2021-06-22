45bba9e829839e0d73798b3a3964892c6ddc755e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-06-22 11:45:09 -0600

    get flipped version of Kimber's data

--- Kimber Simmons/GWAS/kimber_only_qc.sh
+++ Kimber Simmons/GWAS/kimber_only_qc.sh
@@ -8,4 +8,19 @@ plink2 --bfile Data_Cleaned/biobank_analysis/merged_final \
   --make-bed --out Data_Cleaned/kimber_only_analysis/kimber
 # Make ethnicity the outcome
 Rscript ~/GitHub/BDC-Code/Kimber\ Simmons/GWAS/ethnicity_as_outcome.R
+# WD
+cd Data_Cleaned/kimber_only_analysis
 # Convert to VCF for TOPMed upload
+plink2 --bfile kimber --recode vcf --out kimber
+# Compress
+bgzip -c kimber.vcf > kimber.vcf.gz
+tabix -p vcf kimber.vcf.gz
+rm kimber.vcf
+# Split by chromosome
+mkdir chr
+for i in {1..22}
+do
+   bcftools filter kimber.vcf.gz -r $i > chr/chr$i.vcf
+   bgzip -c chr/chr$i.vcf > chr/chr$i.vcf.gz
+   rm chr/chr$i.vcf
+done
