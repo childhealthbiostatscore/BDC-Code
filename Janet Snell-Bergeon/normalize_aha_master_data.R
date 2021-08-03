088de37233340c16dd0a659f37bd1e15fa21d563
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-08-03 15:57:21 -0500

    Update normalize_aha_master_data.R
    normalize untargeted metabolomics

--- Janet Snell-Bergeon/normalize_aha_master_data.R
+++ Janet Snell-Bergeon/normalize_aha_master_data.R
@@ -1,5 +1,6 @@
-setwd("/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Janet Snell-Bergeon/AHA collaborative grant")
+#setwd("/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Janet Snell-Bergeon/AHA collaborative grant")
 # SERRF normalization code
 source("~/GitHub/BDC-Code/Janet Snell-Bergeon/serrf.R")
 # Untargeted metabolomics
-o = normalize("/Users/timvigers/Dropbox/Work/AHA/untargeted_metabolomics_for_SERRF.csv",methods = "SERRF")
+o = normalize("/Users/timvigers/Dropbox/Work/AHA/untargeted_metabolomics_for_SERRF.csv",
+              methods = "SERRF",cores = 6)
