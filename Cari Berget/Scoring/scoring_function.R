26c01348687ba7346d44d4aabeef874d8bba8164
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-10-26 13:54:01 -0600

    corrected CIQ tech scores

--- Cari Berget/Scoring/scoring_function.R
+++ Cari Berget/Scoring/scoring_function.R
@@ -1,3 +1,5 @@
+surveys = c("c_paid","c_hfs_maintain","c_hfs_helpless","c_hfs_worry","c_inspire",
+            "ya_paid","ya_hfs_behave","ya_hfs_worry","adult_inspire","tech")
 score_for_cari = function(df){
   # Score surveys
   scores = t(apply(df,1,function(r){
@@ -24,14 +26,13 @@ score_for_cari = function(df){
     adult_inspire[adult_inspire == 5] = NA
     adult_inspire = mean(adult_inspire,na.rm = T)*25
     # Tech survey
-    tech = sum(as.numeric(r[grep("tech\\d{,2}$",names(r))]))
+    tech = as.numeric(r[grep("tech\\d{,2}$",names(r))]) + 1
+    tech = sum(tech)
     # Return all
     return(c(c_paid,c_hfs_maintain,c_hfs_helpless,c_hfs_worry,c_inspire,
              ya_paid,ya_hfs_behave,ya_hfs_worry,adult_inspire,tech))
   }))
-  colnames(scores) = 
-    c("c_paid","c_hfs_maintain","c_hfs_helpless","c_hfs_worry","c_inspire",
-      "ya_paid","ya_hfs_behave","ya_hfs_worry","adult_inspire","tech")
+  colnames(scores) = surveys
   scores[is.nan(scores)] = NA
   scores = data.frame(scores)
   # Bind
