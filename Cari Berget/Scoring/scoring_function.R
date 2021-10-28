b1ebf6a2938ffba97e27311620a2d2a7c6555a67
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-10-28 10:22:04 -0600

    parent scores

--- Cari Berget/Scoring/scoring_function.R
+++ Cari Berget/Scoring/scoring_function.R
@@ -1,6 +1,6 @@
-surveys = c("c_paid","c_hfs_maintain","c_hfs_helpless","c_hfs_worry","c_inspire",
+peds_surveys = c("c_paid","c_hfs_maintain","c_hfs_helpless","c_hfs_worry","c_inspire",
             "ya_paid","ya_hfs_behave","ya_hfs_worry","adult_inspire","tech")
-score_for_cari = function(df){
+peds_scores = function(df){
   # Score surveys
   scores = t(apply(df,1,function(r){
     # PAID
@@ -32,7 +32,79 @@ score_for_cari = function(df){
     return(c(c_paid,c_hfs_maintain,c_hfs_helpless,c_hfs_worry,c_inspire,
              ya_paid,ya_hfs_behave,ya_hfs_worry,adult_inspire,tech))
   }))
-  colnames(scores) = surveys
+  colnames(scores) = peds_surveys
+  scores[is.nan(scores)] = NA
+  scores = data.frame(scores)
+  # Bind
+  df = cbind(df,scores)
+  return(as.data.frame(df))
+}
+# Parent surveys are in wide format
+parent_scores = function(df){
+  # Score surveys
+  scores = t(apply(df,1,function(r){
+    # PAID
+    p_paid_b = 5 - as.numeric(r[grep("p_paid\\d{,2}$",names(r))])
+    p_paid_t1 = 5 - as.numeric(r[grep("p_paid\\d{,2}_t1$",names(r))])
+    p_paid_t2 = 5 - as.numeric(r[grep("p_paid\\d{,2}_t2$",names(r))])
+    p_paid_t3 = 5 - as.numeric(r[grep("p_paid\\d{,2}_t3$",names(r))])
+    p_paid_t4 = 5 - as.numeric(r[grep("p_paid\\d{,2}_t4$",names(r))])
+    p_paid = list(p_paid_b,p_paid_t1,p_paid_t2,p_paid_t3,p_paid_t4)
+    p_paid = sapply(p_paid,function(x){mean(x,na.rm = T)*25})
+    names(p_paid) = c("p_paid_b","p_paid_t1","p_paid_t2","p_paid_t3","p_paid_t4")
+    # HFS maintain
+    p_hfs_maintain_b =  sum(as.numeric(r[grep("p_hfs_b_behave[347]$",names(r))]))
+    p_hfs_maintain_t1 = sum(5 - as.numeric(r[grep("p_hfs_behave[347]_t1$",names(r))]))
+    p_hfs_maintain_t2 = sum(5 - as.numeric(r[grep("p_hfs_behave[347]_t2$",names(r))]))
+    p_hfs_maintain_t3 = sum(5 - as.numeric(r[grep("p_hfs_behave[347]_t3$",names(r))]))
+    p_hfs_maintain_t4 = sum(5 - as.numeric(r[grep("p_hfs_behave[347]_t4$",names(r))]))
+    p_hfs_maintain = c(p_hfs_maintain_b,p_hfs_maintain_t1,p_hfs_maintain_t2,
+                         p_hfs_maintain_t3,p_hfs_maintain_t4)
+    names(p_hfs_maintain) = c("p_hfs_maintain_b","p_hfs_maintain_t1",
+                                "p_hfs_maintain_t2","p_hfs_maintain_t3",
+                                "p_hfs_maintain_t4")
+    # HFS helpless
+    p_hfs_helpless_b =  sum(as.numeric(r[paste0("p_hfs_b_worry",c(12,13,14,15,17,20,23,24,25,26))]))
+    p_hfs_helpless_t1 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(12,13,14,15,17,20,23,24,25,26),"_t1")]))
+    p_hfs_helpless_t2 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(12,13,14,15,17,20,23,24,25,26),"_t2")]))
+    p_hfs_helpless_t3 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(12,13,14,15,17,20,23,24,25,26),"_t3")]))
+    p_hfs_helpless_t4 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(12,13,14,15,17,20,23,24,25,26),"_t4")]))
+    p_hfs_helpless = c(p_hfs_helpless_b,p_hfs_helpless_t1,p_hfs_helpless_t2,
+                         p_hfs_helpless_t3,p_hfs_helpless_t4)
+    names(p_hfs_helpless) = c("p_hfs_helpless_b","p_hfs_helpless_t1",
+                                "p_hfs_helpless_t2","p_hfs_helpless_t3",
+                                "p_hfs_helpless_t4")
+    # HFS worry
+    p_hfs_worry_b =  sum(as.numeric(r[paste0("p_hfs_b_worry",c(16,18,19,21,22))]))
+    p_hfs_worry_t1 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(16,18,19,21,22),"_t1")]))
+    p_hfs_worry_t2 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(16,18,19,21,22),"_t2")]))
+    p_hfs_worry_t3 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(16,18,19,21,22),"_t3")]))
+    p_hfs_worry_t4 = sum(5 - as.numeric(r[paste0("p_hfs_worry",c(16,18,19,21,22),"_t4")]))
+    p_hfs_worry = c(p_hfs_worry_b,p_hfs_worry_t1,p_hfs_worry_t2,
+                         p_hfs_worry_t3,p_hfs_worry_t4)
+    names(p_hfs_worry) = c("p_hfs_worry_b","p_hfs_worry_t1",
+                                "p_hfs_worry_t2","p_hfs_worry_t3",
+                                "p_hfs_worry_t4")
+    # INSPIRE
+    p_inspire_b = NA
+    p_inspire_t1 = as.numeric(r[grep("p_inspire\\d{,2}_t1$",names(r))])
+    p_inspire_t2 = as.numeric(r[grep("p_inspire\\d{,2}_t2$",names(r))])
+    p_inspire_t3 = as.numeric(r[grep("p_inspire\\d{,2}_t3$",names(r))])
+    p_inspire_t4 = as.numeric(r[grep("p_inspire\\d{,2}_t4$",names(r))])
+    p_inspire = list(p_inspire_b,p_inspire_t1,p_inspire_t2,p_inspire_t3,p_inspire_t4)
+    p_inspire = sapply(p_inspire,function(x){
+      x[x==5] = NA
+      mean(x,na.rm = T)*25
+      })
+    names(p_inspire) = c("p_inspire_b","p_inspire_t1","p_inspire_t2","p_inspire_t3","p_inspire_t4")
+    # Return all
+    return(c(p_paid,p_hfs_maintain,p_hfs_helpless,p_hfs_worry,p_inspire))
+  }))
+  colnames(scores) = gsub("_b","_Baseline",colnames(scores))
+  colnames(scores) = gsub("_t1","_T1",colnames(scores))
+  colnames(scores) = gsub("_t2","_T2",colnames(scores))
+  colnames(scores) = gsub("_t3","_T3",colnames(scores))
+  colnames(scores) = gsub("_t4","_T4",colnames(scores))
   scores[is.nan(scores)] = NA
   scores = data.frame(scores)
   # Bind
