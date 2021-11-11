d6f8c2747e21ef2a081d82ca4529f420b8589240
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-11-11 11:36:25 -0700

    cut instead of tons of if else

--- Erin Cobry/ATTD abstract 2022/scoring_function.R
+++ Erin Cobry/ATTD abstract 2022/scoring_function.R
@@ -1,42 +1,20 @@
+library(lubridate)
 psqi_scores = function(df){
   # Score surveys
   scores = t(apply(df,1,function(r){
-    comp1 = as.numeric(r["psqi_6t"])
-    # Q2
-    q2 = as.character(r["psqi_2"])
-    if(q2 == ""){q2 = NA} else {q2 = as.numeric(lubridate::hm(q2))/60}
-    if(!is.na(q2)){
-      if(q2 <= 15){c2 = 0} 
-      else if (q2 <= 30){c2 = 1} 
-      else if (q2 <= 60){c2 = 2} 
-      else if (q2 > 60){c2 = 3}
-    } else {c2 = NA}
-    # q5a
-    q5a = as.character(r["psqi_3"])
-    if(q5a == ""){q5a = NA} else {q5a = as.numeric(lubridate::hm(q5a))/60}
-    if(!is.na(q5a)){
-      if(q5a <= 15){c3 = 0} 
-      else if (q5a <= 30){c3 = 1} 
-      else if (q5a <= 60){c3 = 2} 
-      else if (q5a > 60){c3 = 3}
-    } else {c3 = NA}
-    # Comp 2
-    co2 = c2 + c3
-    if(!is.na(co2)){
-      if (co2 == 0){comp2 = 0} 
-      else if (co2 %in% 1:2){comp2 = 1}
-      else if (co2 %in% 3:4){comp2 = 2}
-      else if (co2 > 4){comp2 = 3}
-    } else {comp2 = NA}
+    # Duration
+    psqidurat = as.character(r["psqi_4"])
+    if(psqidurat != ""){
+      psqidurat = as.numeric(as.duration(hm(psqidurat)))/3600
+      psqidurat = cut(psqidurat,c(-Inf,5,6,7,Inf),c(3,2,1,0),right = F)
+      psqidurat = as.numeric(as.character(psqidurat))
+    } else {psqidurat = NA}
+    # Latency
+    psqilaten = as.character(r["psqi_2"])
+    if(psqilaten != ""){
+      psqilaten = as.numeric(as.duration(hm(psqilaten)))/60
+      psqilaten = cut(psqilaten,c(-Inf,15,30,60,Inf),c(0,1,2,3),right = T)
+      psqilaten = as.numeric(as.character(psqilaten))
+    } else {psqilaten = NA}
   }))
-  # Comp3 
-  q4 = as.character(r["psqi_3"])
-  if(q4 == ""){q4 = NA} else {q4 = as.numeric(lubridate::hm(q4))/3600}
-  if(!is.na(q4)){
-    if(q4 > 7){c4 = 0} 
-    else if (q4 > 6 & q4 <= 7){c4 = 1} 
-    else if (q4 > 5 & q4 <= 6){c4 = 2} 
-    else if (q4 <= 5){c4 = 3}
-  } else {c4 = NA}
-  
 }
