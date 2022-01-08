6f92a1e4d94d1dbc930d5f97fc4f1e7a68195d07
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-01-08 09:21:39 -0700

    try CART imputation

--- Janet Snell-Bergeon/impute_aha_data.R
+++ Janet Snell-Bergeon/impute_aha_data.R
@@ -38,7 +38,7 @@ exclude = c("StudyID",snps,colnames(df)[nearZeroVar(df)])
 exclude = unique(exclude)
 t = df %>% select(-all_of(exclude))
 # Impute
-imputed_aha = mice(t,m = 10,printFlag = F,seed = 1017)
+imputed_aha = mice(t,m = 10,method = "cart",printFlag = F,seed = 1017)
 # Reassemble
 imputed_aha = cbind(imputed_aha,df[,c(exclude,outcomes)])
 # Filter to SNPs only
