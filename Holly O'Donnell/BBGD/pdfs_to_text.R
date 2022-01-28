36ad1c3d576e63df4b7674c6ea0f946ef0d822b1
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-01-28 14:12:25 -0700

    Update pdfs_to_text.R

--- Holly O'Donnell/BBGD/pdfs_to_text.R
+++ Holly O'Donnell/BBGD/pdfs_to_text.R
@@ -11,7 +11,7 @@ pdf_cgm_data = function(indir,skip = NULL){
   # Iterate through files
   for (f in 1:length(files)) {
     # Skip if relevant
-    if (f %in% skip){next}
+    #if (f %in% skip){next}
     # Read PDF into list
     pdf = pdf_data(files[f])
     # First page
@@ -20,7 +20,7 @@ pdf_cgm_data = function(indir,skip = NULL){
     df = df %>% arrange(x,y)
     # Get name
     name = paste(gsub('[[:punct:]]','',df$text[which(df$x == 46 & df$y ==3)]),
-                 gsub('[[:punct:]]','',df$text[which(df$x == 85 & df$y ==3)]))
+                 gsub('[[:punct:]]','',df$text[which(df$x > 50 & df$x < 200 & df$y ==3)]))
     # Average and readings
     avg_bg = as.numeric(df$text[which(df$x == 107 & df$y == 186)])
     readings = as.numeric(df$text[which(df$x == 107 & df$y == 206)])
@@ -42,6 +42,32 @@ pdf_cgm_data = function(indir,skip = NULL){
     pdf_summary[f,"bolus_per_day"] = bolus_day
     pdf_summary[f,"carbs_per_day"] = carbs_day
     pdf_summary[f,"carb_entries_per_day"] = entries_day
+    # Second page
+    ## Page as a dataframe, sort by x and y values
+    df = as.data.frame(pdf[[2]])
+    df = df %>% arrange(x,y)
+    # CGM data
+    very_high = df$text[which(df$x == 67 & df$y == 95)]
+    high = df$text[which(df$x == 67 & df$y == 111)]
+    target = df$text[which(df$x == 67 & df$y == 127)]
+    low = df$text[which(df$x == 67 & df$y == 143)]
+    very_low = df$text[which(df$x == 67 & df$y == 159)]
+    avg_sg = as.numeric(df$text[which(df$x == 317 & df$y == 130)])
+    sd_sg = as.numeric(sub("mg/dL","",df$text[which(df$x == 548 & df$y == 94)]))
+    percent_time = df$text[which(df$x == 317 & df$y == 157)]
+    # Add to summary df
+    pdf_summary[f,"very_high"] = very_high
+    pdf_summary[f,"high"] = high
+    pdf_summary[f,"tir"] = target
+    pdf_summary[f,"low"] = low
+    pdf_summary[f,"very_low"] = very_low
+    pdf_summary[f,"avg_sg"] = avg_sg
+    pdf_summary[f,"sd_sg"] = sd_sg
+    pdf_summary[f,"percent_time"] = percent_time
   }
   return(pdf_summary)
 }
+
+pdf_summary = pdf_cgm_data("/Users/timvigers/Dropbox/Mac/Documents/PDFs")
+write.csv(pdf_summary,"/Users/timvigers/Dropbox/Mac/Documents/pdf_summary.csv",
+          row.names = F,na = "")
