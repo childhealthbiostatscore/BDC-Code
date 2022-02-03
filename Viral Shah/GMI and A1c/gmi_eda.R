92c3330bd063e2585e5da572bc00a3a6f5ea60f2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-02-03 15:29:12 -0700

    Update gmi_eda.R

--- Viral Shah/GMI and A1c/gmi_eda.R
+++ Viral Shah/GMI and A1c/gmi_eda.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(segmented)
 df = read.csv("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Viral Shah/GMI and A1c/Data_Raw/gmiData 020222 .csv")
 ggplot(df,aes(x = gluMean,y = a1cLab)) + 
-  geom_point() + xlab("MBG") + ylab("HbA1c") + 
-  theme_bw()
+  geom_point(aes(color = gluHours)) + 
+  xlab("MBG") + ylab("HbA1c") + theme_bw()
 # Segmented model
 mod = lm(a1cLab ~ gluMean,data = df)
-seg = segmented(mod,seg.Z = ~gluMean,npsi = 1)
+seg = segmented(mod,seg.Z = ~gluMean,npsi = 4)
 summary(seg)
 # Add segmented line to plot
 df$pred = predict(seg)
