a3813ad4a54f13d472d762a16202769732b9dae8
laurapyle <laurapyle@gmail.com>
2022-02-03 12:19:08 -0700

    Fix PROMIS scoring

--- Erin Cobry/COVID Quality of Life/score_promis.R
+++ Erin Cobry/COVID Quality of Life/score_promis.R
@@ -13,19 +13,21 @@ promis_peds$Timepoint = factor(promis_peds$Timepoint,levels = c("baseline","3_mo
                                  labels = c("Baseline","3 Month","6 Month","9 Month","12 Month"))
 promis_peds$num_time = recode(promis_peds$Timepoint,"Baseline" = 0,"3 Month" = 3,
                                 "6 Month" = 6,"9 Month" = 9,"12 Month" = 12)
+# reverse score question 2
+promis_peds$child_slept <- promis_peds$child_slept+2*(3-promis_peds$child_slept)
 promis_peds <- promis_peds %>% select(record_id,num_time,Timepoint,child_falling:child_problems_day) %>%
   mutate(disturbance = rowSums(select(.,child_falling:child_trouble_sleep)),
          impairment = rowSums(select(.,child_sleepy:child_problems_day)))
 # need to update this part once Erin sends the corrected t-tables
 # Convert to T scores
-# peds_disturbance_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/peds_sleep_disturbance_short.csv")
-# peds_impairment_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/peds_sleep_impairment_short.csv")
-# promis_peds$disturbance = 
-#   peds_disturbance_short$T.Score[match(promis_peds$disturbance,
-#                                  peds_disturbance$Raw.Summed.Score)]
-# promis_peds$impairment = 
-#   peds_impairment_short$T.Score[match(promis_peds$impairment,
-#                                 peds_impairment$Raw.Summed.Score)]
+peds_disturbance_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/peds_sleep_disturbance_short.csv")
+peds_impairment_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/peds_sleep_impairment_short.csv")
+promis_peds$disturbance = 
+  peds_disturbance_short$T.Score[match(promis_peds$disturbance,
+                                       peds_disturbance_short$Raw.Summed.Score)]
+promis_peds$impairment = 
+ peds_impairment_short$T.Score[match(promis_peds$impairment,
+                                     peds_impairment_short$Raw.Summed.Score)]
 
 
 source("./Erin Cobry/ADA 2022/COVID/Data raw/HealthRelatedQuality-PROMISParentProxy_R_2022-02-01_1916.r")
@@ -39,16 +41,19 @@ promis_parent_proxy$Timepoint = factor(promis_parent_proxy$Timepoint,levels = c(
                                labels = c("Baseline","3 Month","6 Month","9 Month","12 Month"))
 promis_parent_proxy$num_time = recode(promis_parent_proxy$Timepoint,"Baseline" = 0,"3 Month" = 3,
                               "6 Month" = 6,"9 Month" = 9,"12 Month" = 12)
+# reverse score question 2
+promis_parent_proxy$slept_night <- promis_parent_proxy$slept_night+2*(3-promis_parent_proxy$slept_night)
+
 promis_parent_proxy <- promis_parent_proxy %>% select(record_id,num_time,Timepoint,diff_sleep:probl_day) %>%
   mutate(disturbance = rowSums(select(.,diff_sleep:troub_sleep)),
          impairment = rowSums(select(.,sleepy_day:probl_day)))
 # need to update this part once Erin sends the corrected t-tables
 # Convert to T scores
-# parent_disturbance_short = read.csv("./Erin Cobry/ADA 2022/HCL and Sleep/Data clean/parent_proxy_sleep_disturbance.csv")
-# parent_impairment_short = read.csv("./Erin Cobry/ADA 2022/HCL and Sleep/Data clean/parent_proxy_sleep_impairment.csv")
-# promis_parent$disturbance = 
-#   parent_disturbance_short$T.Score[match(promis_parent$disturbance,
-#                                    parent_disturbance$Raw.Summed.Score)]
-# promis_parent$impairment = 
-#   parent_impairment_short$T.Score[match(promis_parent$impairment,
-#                                   parent_impairment$Raw.Summed.Score)]
+parent_disturbance_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/parent_proxy_sleep_disturbance_short.csv")
+parent_impairment_short = read.csv("./Erin Cobry/ADA 2022/COVID/Data clean/parent_proxy_sleep_impairment_short.csv")
+promis_parent_proxy$disturbance = 
+ parent_disturbance_short$T.Score[match(promis_parent_proxy$disturbance,
+                                        parent_disturbance_short$Raw.Summed.Score)]
+promis_parent_proxy$impairment = 
+ parent_impairment_short$T.Score[match(promis_parent_proxy$impairment,
+                                       parent_impairment_short$Raw.Summed.Score)]
