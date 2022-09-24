7a7b34f0e67a7b7fb3ee9e291fed82582cd43dfd
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-09-24 12:35:00 -0600

    difacto all timepoints in one DF

--- Howard Davidson/multiassay_preprocessing.R
+++ Howard Davidson/multiassay_preprocessing.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(caret)
 library(parallel)
 # Import raw
-setwd("Z:/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Howard Davidson/TEDDY data")
+setwd("~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Howard Davidson/TEDDY data")
 raw_data = readRDS("./Data_Raw/TEDDY_MP193_Data/raw_time_data.RDS")
 # ID columns (common to all stored dataframes)
-id_cols = c("y","ID","time")
+id_cols = c("y","id","time")
 # Loop through list, and pull out matrices
 X = lapply(names(raw_data), function(x){
   var = tolower(gsub(" ","_",x))
@@ -13,6 +13,7 @@ X = lapply(names(raw_data), function(x){
   ts = lapply(names(dats), function(t){
     d = dats[[t]]$X # Predictors (variable column numbers)
     y = dats[[t]]$Y
+    colnames(y) = tolower(colnames(y))
     y = y[,c(id_cols)] # All need same columns for y
     d = cbind(d,y)
     if(ncol(dats[[t]]$X) == 1){
@@ -23,8 +24,9 @@ X = lapply(names(raw_data), function(x){
     return(d)
   })
   df = data.frame(do.call(rbind,ts)) # Bind together
-  df = df[df$time == 0,] # time 0 only for now
 })
 names(X) = tolower(gsub(" ","_",names(raw_data)))
+# Turn into long dataset
+df = X %>% reduce(full_join) %>% as.data.frame(.)
 # Save data as a list with a dataframe for each assay
-save(X,file = "./Data_Clean/time_0.RData")
+save(df,file = "./Data_Clean/all_timepoints.RData")
