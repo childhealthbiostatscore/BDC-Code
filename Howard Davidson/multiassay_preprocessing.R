fadd55bd51e86187c93cbcc4e2b3e3706c7d6667
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-09-26 11:15:22 -0600

    Update multiassay_preprocessing.R

--- Howard Davidson/multiassay_preprocessing.R
+++ Howard Davidson/multiassay_preprocessing.R
@@ -1,11 +1,9 @@
 library(tidyverse)
-library(caret)
-library(parallel)
 # Import raw
-setwd("~/UCD/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Howard Davidson/TEDDY data")
+setwd("/Users/timvigers/Documents/Work/BDC/Howard Davidson/TEDDY data")
 raw_data = readRDS("./Data_Raw/TEDDY_MP193_Data/raw_time_data.RDS")
 # ID columns (common to all stored dataframes)
-id_cols = c("y","id","time")
+id_cols = c("y","id","pair","time")
 # Loop through list, and pull out matrices
 X = lapply(names(raw_data), function(x){
   var = tolower(gsub(" ","_",x))
@@ -34,10 +32,11 @@ X = lapply(names(raw_data), function(x){
   df = data.frame(do.call(rbind,ts)) # Bind together
 })
 names(X) = tolower(gsub(" ","_",names(raw_data)))
-names(X$positive_lipidomics)
 # Turn into long dataset, sort
 df = X %>% reduce(full_join) %>% as.data.frame(.)
-df = df %>% select(id,time,y,everything()) %>%
+df = df %>% select(id,pair,time,y,everything()) %>%
   arrange(id,time)
+df$y = as.factor(df$y)
+df$time = as.factor(df$time)
 # Save data as a list with a dataframe for each assay
 save(df,file = "./Data_Clean/all_timepoints.RData")
