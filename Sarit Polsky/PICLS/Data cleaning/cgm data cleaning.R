3c783bff783fbfadd4b394aae86741f1825991b1
laurapyle <laurapyle@gmail.com>
2022-11-01 17:04:14 -0600

    Create cgm data cleaning.R

---
+++ Sarit Polsky/PICLS/Data cleaning/cgm data cleaning.R
@@ -0,0 +1,4 @@
+library(cgmanalysis)
+
+cleandata(inputdirectory = "B:\\Projects\\Sarit Polsky\\PICLS\\Data_Raw\\Final raw CGM files for analysis\\",
+          outputdirectory = "B:\\Projects\\Sarit Polsky\\PICLS\\Data_Raw\\Cleaned CGM files")
