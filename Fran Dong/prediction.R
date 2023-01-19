0c6dc6248cfb12d22711964ec0645a004f49ab45
laurapyle <laurapyle@gmail.com>
2023-01-18 21:27:08 -0700

    Update prediction.R

--- Fran Dong/prediction.R
+++ Fran Dong/prediction.R
@@ -6,3 +6,9 @@ data$SEX <- relevel(data$SEX, ref = "Male")
 
 
 res.cox <- coxph(Surv(fup_t1D2, T1D_case) ~ as.factor(SEX) + as.factor(NHW) + as.factor(T1D_Fam) + HbA1c, data = data)
+
+new <- read.csv("B:/Projects/Fran Dong/ASK predictive model/new.csv")
+
+p1 <- predict(res.cox, data, type="survival")
+
+p2 <- predict(res.cox, newdata = new, type="survival")
