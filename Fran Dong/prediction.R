e9443089349c4f76e1bbdb421beaa279f3e88be7
laurapyle <laurapyle@gmail.com>
2023-01-18 20:04:08 -0700

    Create prediction.R

---
+++ Fran Dong/prediction.R
@@ -0,0 +1,8 @@
+library(survival)
+
+data <- sas7bdat::read.sas7bdat("B:/Projects/Fran Dong/ASK predictive model/iacombined01062023.sas7bdat")
+data$SEX <- as.factor(data$SEX)
+data$SEX <- relevel(data$SEX, ref = "Male")
+
+
+res.cox <- coxph(Surv(fup_t1D2, T1D_case) ~ as.factor(SEX) + as.factor(NHW) + as.factor(T1D_Fam) + HbA1c, data = data)
