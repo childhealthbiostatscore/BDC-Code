f0d85b191beabff37ed8f28591979db9f0f01e8d
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-02-14 16:48:06 -0700

    drier code

--- Sarit Polsky/PICLS/count_fake_carbs.py
+++ Sarit Polsky/PICLS/count_fake_carbs.py
@@ -4,11 +4,11 @@ from glob import iglob
 from os.path import basename
 import pandas as pd
 import numpy as np
-wd = "/Volumes/som/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Sarit Polsky/PICLS/"
+wd = "/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Projects/Sarit Polsky/PICLS/"
 # Time range
 time_range = "2 min"
 # Dict for results
-res = {"file": [], "num_other": [], "one_bolus": [], "zero_bolus": [],
+res = {"file": [], "num_other_events": [], "one_bolus": [], "zero_bolus": [],
        "more_than_one_bolus": [], "more_than_one_carb": []}
 # Iterate through all files
 for file in iglob(wd + "Data_Raw/CGM Downloads/" + "**/*.csv", recursive=True):
@@ -36,14 +36,12 @@ for file in iglob(wd + "Data_Raw/CGM Downloads/" + "**/*.csv", recursive=True):
     # 'Other' event timestamps
     other_times = list(df["timestamp"].iloc[list(
         np.where(df["Event Marker"] == "Other")[0])].dropna())
-    # Count the number of boluses associated with each 'Other' event
+    # Count the number of carbs and boluses associated with each 'Other' event
     num_bolus = []
+    num_carbs = []
     for t in other_times:
         num_bolus.append(len([b for b in bolus_times if b < t + pd.to_timedelta(
             time_range) and b > t - pd.to_timedelta(time_range)]))
-    # Count the number of carbs associated with each 'Other' event
-    num_carbs = []
-    for t in other_times:
         num_carbs.append([c for c in carb_times if c < t +
                          pd.to_timedelta(time_range) and c > t - pd.to_timedelta(time_range)])
     # Convert any multiple carbs timestamps into a single string ()
@@ -51,7 +49,7 @@ for file in iglob(wd + "Data_Raw/CGM Downloads/" + "**/*.csv", recursive=True):
     multiple_carbs = ", ".join([str(i) for i in list(
         itertools.chain.from_iterable(multiple_carbs))])
     # Store results
-    res["num_other"].append(len(other_times))
+    res["num_other_events"].append(len(other_times))
     res["one_bolus"].append(len([b for b in num_bolus if b == 1]))
     res["zero_bolus"].append(len([b for b in num_bolus if b == 0]))
     res["more_than_one_bolus"].append(len([b for b in num_bolus if b > 1]))
