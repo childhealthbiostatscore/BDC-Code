8c18c2006985fdbb97c57a055f0164bc7ef5b591
laurapyle <laurapyle@gmail.com>
2023-09-27 13:23:02 -0600

    double checking excluded values

--- Todd Alonso/4Ts/merge_data.R
+++ Todd Alonso/4Ts/merge_data.R
@@ -38,6 +38,63 @@ alldata <- alldata %>% select(-c("MRN","DOB","Gender","Language_EPIC","OnsetDate
 
 # set one A1c value at onset to missing
 alldata$hba1c_onset <- ifelse(alldata$hba1c_onset==618,NA,alldata$hba1c_onset)
+alldata$days_to_cgm <- as.numeric(alldata$days_to_cgm)
+alldata$days_to_pump <- as.numeric(alldata$days_to_pump)
 
 # write file
 write.csv(alldata,"/Volumes/BDC/Projects/Todd Alonso/4Ts analysis/Data_clean/merged_data.csv", row.names = F)
+
+# checking number of excluded records
+dat_excl <- subset(alldata, !(cohort %in% c("hist", "pres")) |
+                     (age_at_onset < 0.5 | age_at_onset >= 21) |
+                     !(sex %in% c(NA, "Female", "Male")) |
+                     !(race_ethnicity %in% c("Non-Hispanic White", "Non-Hispanic Black",
+                                             "Hispanic", "Asian or Pacific Islander",
+                                             "American Indian or Alaska Native", "Other",
+                                             "Unknown / Declined to state")) |
+                     !(dka_onset %in% c(NA, 0:1)) |
+                     !(insurance_type %in% c(NA, "Private", "Public", "Both", "No Insurance")) |
+                     !(language %in% c(NA, "English", "Non-English")) |
+                     studyday < 0 |
+                     (hba1c_onset < 4 | hba1c_onset > 20) |
+                     (hba1c < 4 | hba1c > 20) |
+                     !(cgm_initiated %in% 0:1) |
+                     !(cgm_initiated_30d %in% c(NA, 0:1)) |             # unavailable at BDC
+                     (days_to_cgm < 0 | days_to_cgm > 360) |  # unavailable at BDC
+                     !(pump_yesno %in% c(NA, 0:1)) |
+                     !(pump_plgs %in% 0:1) |
+                     !(pump_open_loop %in% 0:1) |
+                     !(pump_hybrid_cl %in% 0:1) |
+                     !(pump_adv_hybrid_cl %in% 0:1) |
+                     (days_to_pump < 0 | days_to_pump > 360)) # unavailable at BDC
+
+data_exl_cohort <- data_exl_cohort %>% filter(!(cohort %in% c("hist", "pres")))
+data_exl_age <- subset(alldata, (age_at_onset < 0.5 | age_at_onset >= 21) )
+data_exl_sex <- subset(alldata, !(sex %in% c(NA, "Female", "Male")))
+data_exl_race <- subset(alldata, !(race_ethnicity %in% c("Non-Hispanic White", "Non-Hispanic Black",
+                                                           "Hispanic", "Asian or Pacific Islander",
+                                                           "American Indian or Alaska Native", "Other",
+                                                           "Unknown / Declined to state")))
+data_exl_dka <- subset(alldata, !(dka_onset %in% c(NA, 0:1)))
+data_exl_ins <- subset(alldata, !(insurance_type %in% c(NA, "Private", "Public", "Both", "No Insurance")))
+data_exl_lang <- subset(alldata, !(language %in% c(NA, "English", "Non-English")))
+data_exl_studyday <- subset(alldata, studyday < 0)
+data_exl_a1conset <- subset(alldata, (hba1c_onset < 4 | hba1c_onset > 20))
+data_exl_a1c <- subset(alldata, (hba1c < 4 | hba1c > 20))
+data_exl_cgminit <- subset(alldata,  !(cgm_initiated %in% 0:1))
+data_exl_cgminit30 <- subset(alldata, !(cgm_initiated_30d %in% c(NA, 0:1)))
+data_exl_daystocgm <- subset(alldata, (days_to_cgm < 0 | days_to_cgm > 360))
+data_exl_pump <- subset(alldata, !(pump_yesno %in% c(NA, 0:1)))
+data_exl_plgs <- subset(alldata, !(pump_plgs %in% 0:1) )
+data_exl_open <- subset(alldata, !(pump_open_loop %in% 0:1) )
+data_exl_hcl <- subset(alldata, !(pump_hybrid_cl %in% 0:1))
+data_exl_advhcl <- subset(alldata, !(pump_adv_hybrid_cl %in% 0:1))
+data_exl_daystopump <- subset(alldata, (days_to_pump < 0 | days_to_pump > 360))
+
+# summary of excluded values
+# 33 American Indian/Alaska Native
+# 1508 days_to_cgm were excluded, until I converted to numeric and then none were
+# all values of pump_open_loop are missing
+# all values of pump_hybrid_cl are missing
+# all values of pump_adv_hybrid_cl are missing
+# 584 days_to_pump were excluded until I converted to numeric and then none were
