493b0cd4d049cd5f31fad86c1e309024ed9e31ba
Tim Vigers <timothy.vigers@cuanschutz.edu>
2023-12-12 17:45:47 -0700

    analyte file as CSV

--- Janet Snell-Bergeon/DCCT and EDIC/create_omics_linkage_file.R
+++ Janet Snell-Bergeon/DCCT and EDIC/create_omics_linkage_file.R
@@ -4,10 +4,10 @@ setwd("~/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/BDC/Janet S
 # Set up biomaRt for annotation
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
 # Untargeted metabolomics
-metabolite_annotation <- read.csv("./Metabolomics/EDIC metabolomics final data matrix.csv",
+metabs <- read.csv("./Metabolomics/EDIC metabolomics final data matrix.csv",
   stringsAsFactors = F, na.strings = c("", "NA")
 )
-metabolite_annotation <- metabolite_annotation %>%
+metabs <- metabs %>%
   mutate(
     compound = coalesce(NovaMT.Library.No., External.Identifier),
     m.z = Neutral.Mass..Da. + 1.0078,
@@ -16,38 +16,51 @@ metabolite_annotation <- metabolite_annotation %>%
     rterror = RT.Error..s.
   ) %>%
   select(compound, m.z, rt, masserror, rterror)
-metabolite_annotation$name <- paste(
-  metabolite_annotation$compound,
-  metabolite_annotation$m.z,
-  metabolite_annotation$rt,
+metabs$name <- paste(
+  metabs$compound,
+  metabs$m.z,
+  metabs$rt,
   sep = "_"
 )
-metabolite_annotation <- metabolite_annotation %>% select(name, everything())
+metabs <- metabs %>% select(name, everything())
+metabs$Platform = "Untargeted Metabolomics"
 # Import proteomics
 proteins <- read.delim("./Proteomics/Raw data/EDIC_WP_DDA_60SPD_Proteins.txt")
 proteins <- proteins$Accession
 # Find Entrez IDs
 p <- getBM(
-  attributes = c("uniprot_gn_id", "entrezgene_id"),
+  attributes = c("uniprot_gn_id", "ensembl_gene_id","entrezgene_id"),
   filters = "uniprot_gn_id", values = proteins,
   mart = ensembl
 )
 # Check isoforms
 p2 <- getBM(
-  attributes = c("uniprot_isoform", "entrezgene_id"),
+  attributes = c("uniprot_isoform","ensembl_gene_id", "entrezgene_id"),
   filters = "uniprot_isoform",
   values = proteins[!proteins %in% p$uniprot_gn_id],
   mart = ensembl
 )
 # Check Swiss-Prot
 p3 <- getBM(
-  attributes = c("uniprotswissprot", "entrezgene_id"),
+  attributes = c("uniprotswissprot","ensembl_gene_id", "entrezgene_id"),
   filters = "uniprotswissprot",
   values = proteins[!proteins %in% c(p$uniprot_gn_id, p2$uniprot_isoform)],
   mart = ensembl
 )
-cnames <- c("UniProt", "Entrez")
-protein_anotation <- rbind(setNames(p, cnames), setNames(p2, cnames), setNames(p3, cnames))
+cnames <- c("name","Ensembl", "Entrez")
+prots <- rbind(setNames(p, cnames), setNames(p2, cnames), setNames(p3, cnames))
+prots$Platform = "Proteomics"
+# Lipidomics
+lipids = read.csv("./Lipidomics/Data_raw/EDIC lipidomics data matrix_Skyline_2021.02.25.csv")
+lipids$Platform = "Lipidomics"
+lipids = lipids %>% select(LipidSpecies,Platform) %>% rename(name = LipidSpecies)
+# Combine and format
+df = full_join(metabs,prots,by = join_by(name, Platform))
+df = full_join(df,lipids,by = join_by(name, Platform))
+df = df %>%
+  select(name,Platform,compound:rterror,Entrez,Ensembl) %>%
+  rename(Name = name,Compound = compound,`M/Z` = m.z,
+         RT = rt,`Mass Error` = masserror,`RT Error` = rterror,
+         `Entrez Gene ID` = Entrez,`Ensembl Gene ID`)
 # Save
-save(metabolite_annotation,protein_anotation,
-     file = "./Data_Raw/annotations.RData")
+write.csv(df,file = "Data_Clean/DCCT-EDIC Analyte Info.csv",na="",row.names = F)
