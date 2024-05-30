df18614332f98416ae9919a396a1b517dfd47bb5
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-05-30 14:41:01 -0600

    formatting

--- Janet Snell-Bergeon/MERIT/redcap_sync.R
+++ Janet Snell-Bergeon/MERIT/redcap_sync.R
@@ -17,4 +17,7 @@ db_randomization_of_exercise <- db_randomization_of_exercise %>%
   rename(participant_id = record_id) %>%
   select(participant_id, exercise_order)
 # Import into MERIT database
-importRecords(rcon = merit, data = castForImport(db_randomization_of_exercise,merit))
+importRecords(
+  rcon = merit,
+  data = castForImport(db_randomization_of_exercise, merit)
+)
