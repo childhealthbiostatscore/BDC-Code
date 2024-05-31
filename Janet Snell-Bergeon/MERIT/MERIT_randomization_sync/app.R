c3ff6f9c8d7050eef3250a616742877bb49da05a
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-05-31 12:28:57 -0600

    MERIT sync shiny app (NO API KEYS)

---
+++ Janet Snell-Bergeon/MERIT/MERIT_randomization_sync/app.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(tidyverse)
+library(REDCapR)
+library(DT)
+ui <- fluidPage(
+  actionButton("sync", "Click here to sync!"),
+  DTOutput("done")
+)
+server <- function(input, output) {
+  diffs <- eventReactive(input$sync, {
+    # Pull data from the exercise database
+    exercise <- redcap_read(
+      redcap_uri = "https://redcap.ucdenver.edu/api/",
+      token = "",
+      batch_size = 10e6, verbose = F,
+      fields = c("record_id", "exercise_order")
+    )$data
+    exercise <- exercise %>% rename(participant_id = record_id)
+    # Pull from MERIT
+    merit <- redcap_read(
+      redcap_uri = "https://redcap.ucdenver.edu/api/",
+      token = "",
+      batch_size = 10e6, verbose = F,
+      fields = c("participant_id", "exercise_order")
+    )$data
+    # Get differences for printing
+    diffs <- setdiff(exercise, merit)
+    # Import into MERIT database
+    redcap_write_oneshot(exercise,
+      redcap_uri = "https://redcap.ucdenver.edu/api/",
+      token = "", verbose = F
+    )
+    as.data.frame(diffs)
+  })
+  output$done <- renderDT(diffs())
+}
+shinyApp(ui, server)
