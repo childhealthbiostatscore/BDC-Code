544e678d5c5641e866d86156c2ec676cc41e3f2c
Tim Vigers <timothy.vigers@cuanschutz.edu>
2024-08-22 16:59:26 -0600

    nutrient FFQs column by column

--- Janet Snell-Bergeon/PROMISE/nutrient_calculations.R
+++ Janet Snell-Bergeon/PROMISE/nutrient_calculations.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(readxl)
 library(redcapAPI)
+library(fastDummies)
 setwd("/home/tim/OneDrive/Vigers/BDC/Janet Snell-Bergeon/PROMISE")
 # Import FFQ data from REDCap
 unlockREDCap(c(rcon = "The PROMISE Study"),
-             keyring = "API_KEYs",
-             envir = 1,
-             url = "https://redcap.ucdenver.edu/api/"
+  keyring = "API_KEYs",
+  envir = 1,
+  url = "https://redcap.ucdenver.edu/api/"
 )
-ffqs = exportReportsTyped(rcon, report_id = 119506)
+ffqs <- exportReportsTyped(rcon, report_id = 119506)
 # Import nutrient tables
 cereal <- read_excel("./Data_Raw/CEREAL-NUTRIENT-TABLE-2022_Updated.xlsx")
 food <- read_excel("./Data_Raw/Nutrient Tables/FOOD-NUTRIENT-TABLE-2022.xlsx")
@@ -16,8 +17,15 @@ marg <- read_excel("./Data_Raw/Nutrient Tables/Margarine-Nutrient-Table-2022.xls
 oil <- read_excel("./Data_Raw/Oil-Nutrient-Table-2022_Updated.xlsx")
 oxalates <- read_excel("./Data_Raw/Nutrient Tables/OXALATE-TABLE.xlsx")
 vitamins <- read_excel("./Data_Raw/Nutrient Tables/VITAMIN-NUTRIENT-TABLE-2022.xlsx")
-# Unfortunately we may have to go section by section and apply different rules
+#-------------------------------------------------------------------------------
+# Vitamins
+#-------------------------------------------------------------------------------
 
-# Merge
-nutrients <- full_join(food, marg)
-nutrients <- full_join(nutrients, oil)
+# Prepare the numeric columns
+levels(ffqs$how_many_multi_vitamins_do) <- c(1 / 7, 4 / 7, 7.5 / 7, 10 / 7)
+ffqs$how_many_multi_vitamins_do <-
+  as.numeric(as.character(ffqs$how_many_multi_vitamins_do))
+ffqs$how_many_multi_vitamins_do[is.na(ffqs$how_many_multi_vitamins_do)] = 0
+# I can't think of a more efficient way to do this, so for now it'll have to 
+# an apply statement
+
