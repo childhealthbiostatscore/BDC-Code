713b2672f1bab302588f2a0a24a5f695703647b3
Tim Vigers <timothy.vigers@cuanschutz.edu>
2025-08-20 09:34:37 -0600

    cleaned new data from Fran

--- Andrea Steck/Advanced CGM and OGTT/create_analysis_dataset.R
+++ Andrea Steck/Advanced CGM and OGTT/create_analysis_dataset.R
@@ -2,74 +2,76 @@ library(tidyverse)
 library(readxl)
 library(haven)
 library(hms)
-# home_dir <- switch(Sys.info()["sysname"],
-#   "Darwin" = "/Users/tim/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT",
-#   "Windows" = "C:/Users/Tim/OneDrive - The University of Colorado Denver/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT",
-#   "Linux" = "/home/tim/OneDrive/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT"
-# )
-user <- Sys.info()[["user"]]
-if (user == "laurapyle") {
-  home_dir <- "/Users/laurapyle/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT"
-} else if (user == "lpyle") {
-  home_dir <- "/Users/lpyle/Library/CloudStorage/OneDrive-UW/Bjornstad/Biostatistics Core Shared Drive/TODAY subaward"
-} else {
-  stop("Unknown user: please specify root path for this user.")
-}
+home_dir <- switch(
+  Sys.info()[["user"]],
+  "laurapyle" = "/Users/laurapyle/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT",
+  "lpyle" = "/Users/lpyle/Library/CloudStorage/OneDrive-UW/Bjornstad/Biostatistics Core Shared Drive/TODAY subaward",
+  "tim" = "/Users/tim/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Vigers/BDC/Andrea Steck/Advanced CGM and OGTT"
+)
 setwd(home_dir)
 # Import CGM data from Fran
-cgm <- read_sas("./Data_Raw/Final data20250801/tim_allcgmclean_gt4days.sas7bdat")
+cgm <- read_sas(
+  "./Data_Raw/Final data20250801/tim_allcgmclean_gt4days.sas7bdat"
+)
+# Import demographics
+demo <- read_sas("./Data_Raw/Final data20250801/demo270.sas7bdat")
+add_demo <- read_sas("./Data_Raw/Final data20250801/adddata.sas7bdat")
 # Convert from numeric time to datetime, round to nearest 5 minutes
-cgm$sensordisplaytime <-
-  as.POSIXct(cgm$newsensortime, origin = "1960-01-01")
+cgm$sensordisplaytime <- as.POSIXct(cgm$newsensortime, origin = "1960-01-01")
 cgm$sensordisplaytime <- round_date(cgm$sensordisplaytime, "5 minutes")
 # Separate date and time columns, format
 cgm$Date <- as_date(cgm$sensordisplaytime)
 cgm$Time <- as_hms(cgm$sensordisplaytime)
-cgm <- cgm %>%
-  rename(SensorValue = sensor_glucose, DOVISIT = dov_CGM) %>%
-  arrange(ID, sensordisplaytime) %>%
-  select(ID, Date, Time, DOVISIT, SensorValue)
+cgm <- cgm %>% rename(SensorValue = sensor_glucose)
 # For now, if people have duplicated sensor values, take the mean
 cgm <- cgm %>%
-  group_by(ID, Date, Time, DOVISIT) %>%
-  summarise(SensorValue = mean(SensorValue, na.rm = T), .groups = "drop")
-# Import demographic, etc. data
-df <- read_sas("./Data_Raw/Final data20250801/demo270.sas7bdat")
-#df <- read_excel("./Data_Raw/Final data for CGM analyses 09012023/final data including TESS.xls", na = ".")
-# Remove unhelpful rows
-df <- df %>% filter(!is.na(ID))
+  group_by(ID, study, dov_CGM, Date, Time) %>%
+  summarise(SensorValue = mean(SensorValue, na.rm = TRUE), .groups = "drop") %>%
+  arrange(ID, Date, Time)
+# Merge
+cgm <- full_join(cgm, demo, by = join_by(ID, study))
+cgm <- full_join(cgm, add_demo, by = join_by(ID, study, dov_CGM))
 # Determine group
-df$Group <- factor(is.na(df$`EventVisDt_t1d`),
-  levels = c(T, F),
+cgm$Group <- factor(
+  cgm$case_T1D,
+  levels = c(0, 1),
   labels = c("Non-Progressor", "Progressor")
 )
-# Convert columns
-df$`age at clinic` <- as.numeric(df$lastage)
-df$DOVISIT <- ymd(df$lastVisDt)
-
-# LP stopped here - missing weight, BMI-Z, HbA1c
-df$`calculated body` = as.numeric(df$`calculated body`)
-df$`BMI-for-age Z`=as.numeric(df$`BMI-for-age Z`)
-# Calculate final visit date (or T1D progression)
-df$final_visit <- df$DOVISIT - round(df$yearsfromT1D * 365.25)
-# Select only necessary columns
-df <- df %>%
-  select(
-    ID, DOVISIT, A1C, `calculated body`, `BMI-for-age Z`, `age at clinic`, SEX,
-    `FDR status`, Race_Ethn2, HLAGRP, Group, final_visit
-  )
-# Put together
-cgm <- left_join(cgm, df, by = join_by(ID, DOVISIT))
-# Sort
-cgm <- cgm %>% arrange(ID, Date, Time)
-# Calculate time from last visit, order columns, remove rows missing CGM value
+# Format dates, etc.
+cgm$DOVISIT <- ymd(cgm$DOVISIT)
+cgm$EventVisDt_t1d <- ymd(cgm$EventVisDt_t1d)
+cgm$lastVisDt <- ymd(cgm$lastVisDt)
+# Calculate days from progression or last visit
 cgm <- cgm %>%
-  mutate(Days = as.numeric(difftime(Date, final_visit, units = "days"))) %>%
+  mutate(
+    DaysFromEndpoint = case_when(
+      Group == "Progressor" ~ difftime(Date, EventVisDt_t1d, units = "days"),
+      Group == "Non-Progressor" ~ difftime(Date, lastVisDt, units = "days")
+    )
+  )
+# Order and select columns
+cgm <- cgm %>%
   select(
-    ID, Group, SEX, `FDR status`, HLAGRP, Race_Ethn2, final_visit, DOVISIT,
-    `age at clinic`, A1C, `calculated body`, `BMI-for-age Z`, Days, Date, Time,
+    ID,
+    study,
+    Group,
+    sex,
+    Race_Ethn2,
+    screen_FDR_GP,
+    HLAGRP,
+    DOB,
+    Date,
+    EventVisDt_t1d,
+    lastVisDt,
+    DaysFromEndpoint,
+    A1C,
+    bmi,
+    bmiz,
+    DOVISIT,
+    dov_CGM,
+    Date,
+    Time,
     SensorValue
-  ) %>%
-  drop_na(SensorValue)
+  )
 # Save
 save(cgm, file = "./Data_Clean/analysis_dataset.RData")
