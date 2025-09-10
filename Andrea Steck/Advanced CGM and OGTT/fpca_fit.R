a9ba36220d6d09d7cfe3acd9c559fe521b148885
Tim Vigers <timothy.vigers@cuanschutz.edu>
2025-09-10 11:26:26 -0700

    trying progressors and non-progressors separately

--- Andrea Steck/Advanced CGM and OGTT/fpca_fit.R
+++ Andrea Steck/Advanced CGM and OGTT/fpca_fit.R
@@ -13,11 +13,30 @@ setwd(home_dir)
 load(file = "./Data_Clean/analysis_dataset.RData")
 # We want to use sparse/irregular FPCA for these data. For now, we'll set the
 # domain as time to last visit
-sparse_cgm <- cgm %>%
+sparse_cgm_prog <- cgm %>%
+    filter(Group == "Progressor") %>%
     select(ID, TimeFromEndpoint, SensorValue) %>%
-    filter(TimeFromEndpoint < 0) %>%
-    mutate(TimeFromEndpoint = -1 * TimeFromEndpoint) %>%
     rename(argvals = TimeFromEndpoint, subj = ID, y = SensorValue) %>%
     drop_na()
-face_fit <- face.sparse(sparse_cgm, calculate.scores = TRUE, pve = 0.95)
-save(face_fit, sparse_cgm, file = "./Data_Clean/face_fit_sparse_cgm.RData")
+sparse_cgm_non_prog <- cgm %>%
+    filter(Group == "Non-Progressor") %>%
+    select(ID, TimeFromEndpoint, SensorValue) %>%
+    rename(argvals = TimeFromEndpoint, subj = ID, y = SensorValue) %>%
+    drop_na()
+face_fit_prog <- face.sparse(
+    sparse_cgm_prog,
+    calculate.scores = TRUE,
+    two_step = TRUE
+)
+face_fit_non_prog <- face.sparse(
+    sparse_cgm_non_prog,
+    calculate.scores = TRUE,
+    two_step = TRUE
+)
+save(
+    face_fit_prog,
+    sparse_cgm_prog,
+    face_fit_non_prog,
+    sparse_cgm_non_prog,
+    file = "./Data_Clean/face_fit_sparse_cgm.RData"
+)
