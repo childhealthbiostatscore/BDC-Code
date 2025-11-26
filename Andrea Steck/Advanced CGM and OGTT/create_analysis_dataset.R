03ccfb1d3cca835ea75929d6615d36deeb825ec2
Tim Vigers <tvigers@uw.edu>
2025-11-26 10:10:03 -0800

    moved joint model data prep into create_analysis_dataset

--- Andrea Steck/Advanced CGM and OGTT/create_analysis_dataset.R
+++ Andrea Steck/Advanced CGM and OGTT/create_analysis_dataset.R
@@ -84,6 +84,90 @@ cgm <- cgm %>%
 cgm$sex <- factor(cgm$sex)
 cgm$screen_FDR_GP <- factor(cgm$screen_FDR_GP)
 cgm$study <- factor(cgm$study)
+# Data for joint models
+# Calculate age and days from progression or last visit, and time from first CGM
+cgm <- cgm |>
+  mutate(
+    Age = as.numeric(difftime(dov_CGM, DOB, units = "days")) / 365.25,
+    AgeEndpoint = as.numeric(difftime(LastVisitDate, DOB, units = "days")),
+    CGMDaysFromEndpoint = as.numeric(difftime(
+      dov_CGM,
+      LastVisitDate,
+      units = "days"
+    ))
+  ) |>
+  group_by(ID, study) |>
+  mutate(
+    FirstCGMDate = first(dov_CGM),
+    TimeFromFirstCGM = as.numeric(difftime(
+      dov_CGM,
+      FirstCGMDate,
+      units = "days"
+    )),
+    TimeOfEndpoint = as.numeric(difftime(
+      LastVisitDate,
+      FirstCGMDate,
+      units = "days"
+    ))
+  ) |>
+  ungroup()
+# Exclude 05-01813's weird 2019-12-17 CGM per Brigs
+cgm <- cgm |> filter(!(ID == "05-01813" & dov_CGM == "2019-12-17"))
+# Time periods per Andrea and Brigs
+cgm <- cgm |>
+  mutate(
+    nTime = as.numeric(Time),
+    TimePeriod = case_when(
+      nTime >= 82800 | nTime < 18000 ~ "11pm - 5am",
+      nTime >= 18000 & nTime < 39600 ~ "5am - 11am",
+      nTime >= 39600 & nTime < 61200 ~ "11am - 5pm",
+      nTime >= 61200 & nTime < 82800 ~ "5pm - 11pm",
+      .default = "other"
+    ),
+    # Shift numeric time so that 11pm is our 0
+    nTime = ifelse(nTime >= 82800, nTime - 82800, nTime + 3600)
+  )
+cgm$TimePeriod <- factor(
+  cgm$TimePeriod,
+  levels = c("11pm - 5am", "5am - 11am", "11am - 5pm", "5pm - 11pm")
+)
+# For each CGM wear, calculate summary metrics for LMMs
+cgm_lmm <- cgm |>
+  filter(CGMDaysFromEndpoint <= 0) |>
+  group_by(
+    ID,
+    Group,
+    sex,
+    Race_Ethn2,
+    screen_FDR_GP,
+    maxAB_group,
+    Age
+  ) |>
+  summarise(
+    mean_glucose = mean(SensorValue, na.rm = TRUE),
+    sd_glucose = sd(SensorValue, na.rm = TRUE),
+    cv_glucose = sd_glucose / mean_glucose,
+    perc_time_over_140 = round(mean(SensorValue > 140, na.rm = T) * 100),
+    hba1c = unique(A1C),
+    .groups = "drop"
+  ) |>
+  drop_na()
+# Create survival dataset
+cgm_surv <- cgm |>
+  group_by(
+    ID,
+    Group,
+    sex,
+    Race_Ethn2,
+    screen_FDR_GP,
+    maxAB_group
+  ) |>
+  summarise(AgeEndpoint = unique(AgeEndpoint) / 365.25, .groups = "drop") |>
+  rename(Ages = AgeEndpoint) |>
+  filter(ID %in% cgm_lmm$ID)
+cgm_surv$event = as.numeric(cgm_surv$Group == "Progressor")
+# Drop unused levels
+cgm_lmm$maxAB_group <- droplevels(cgm_lmm$maxAB_group)
+cgm_surv$maxAB_group <- droplevels(cgm_surv$maxAB_group)
 # Save
-write.csv(cgm, file = "./Data_Clean/cgm.csv", row.names = F, na = "")
-save(cgm, file = "./Data_Clean/analysis_dataset.RData")
+save(cgm, cgm_lmm, cgm_surv, file = "./Data_Clean/analysis_dataset.RData")
