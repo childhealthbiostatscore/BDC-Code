0b7bb8497326eee7a623e6e9a580ab6677cafb98
Tim Vigers <tvigers@uw.edu>
2025-12-01 16:33:29 -0800

    trying Hyak again

--- Andrea Steck/Advanced CGM and OGTT/joint_models.r
+++ Andrea Steck/Advanced CGM and OGTT/joint_models.r
@@ -10,21 +10,23 @@ base_dir <- switch(
   "togo" = "/home/tvigers/Documents/Data",
   "Tims-MacBook-Air.local" = "/Users/tim/Library/CloudStorage/OneDrive-UW",
   "Tims-Mac-mini.local" = "/Users/tim/Library/CloudStorage/OneDrive-UW",
-  "Mac" = "/Users/tim/Library/CloudStorage/OneDrive-UW"
+  "Mac" = "/Users/tim/Library/CloudStorage/OneDrive-UW",
+  "/gscratch/togo/tvigers/Data"
 )
 data_dir <- switch(
   Sys.info()["nodename"],
   "togo" = "/BDC/Andrea Steck/Advanced CGM and OGTT",
   "Tims-MacBook-Air.local" = "/UWMDI/Andrea Steck/Advanced CGM and OGTT",
   "Tims-Mac-mini.local" = "/UWMDI/Andrea Steck/Advanced CGM and OGTT",
-  "Mac" = "/UWMDI/Andrea Steck/Advanced CGM and OGTT"
+  "Mac" = "/UWMDI/Andrea Steck/Advanced CGM and OGTT",
+  "/BDC/Andrea Steck/Advanced CGM and OGTT"
 )
 setwd(paste0(base_dir, data_dir))
 # See BDC-Code/Andrea Steck/Advanced CGM and OGTT/create_analysis_dataset.R
 load("./Data_Clean/analysis_dataset.RData")
 # Filter out age > 30
-# cgm_lmm = cgm_lmm |> filter(Age <= 30)
-# cgm_surv = cgm_surv |> filter(ID %in% unique(cgm_lmm$ID))
+# cgm_lmm <- cgm_lmm |> filter(Age <= 30)
+# cgm_surv <- cgm_surv |> filter(ID %in% unique(cgm_lmm$ID))
 # Cox model
 cox_fit <- coxph(
   Surv(Ages, event) ~ sex +
@@ -35,7 +37,6 @@ cox_fit <- coxph(
   model = T,
   data = cgm_surv
 )
-save(cox_fit, file = "./Data_Clean/Joint Model Results/All/cox_fit.RData")
 # Mean glucose
 lme_fit_mean_rs <- lme(
   mean_glucose ~ Age,
@@ -47,16 +48,10 @@ joint_fit_mean <- jm(
   lme_fit_mean_rs,
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save and clean up memory
-save(
-  joint_fit_mean,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_mean.RData"
-)
-rm(joint_fit_mean)
 # SD
 lme_fit_sd_rs <- lme(
   sd_glucose ~ Age,
@@ -69,34 +64,20 @@ joint_fit_sd <- jm(
   lme_fit_sd_rs,
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save and clean up memory
-save(
-  joint_fit_sd,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_sd.RData"
-)
-rm(joint_fit_sd)
 # Both
 joint_fit_mean_sd <- jm(
   cox_fit,
   list(lme_fit_mean_rs, lme_fit_sd_rs),
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save and clean up memory
-save(
-  lme_fit_mean_rs,
-  lme_fit_sd_rs,
-  joint_fit_mean_sd,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_mean_sd.RData"
-)
-rm(lme_fit_mean_rs, lme_fit_sd_rs, joint_fit_mean_sd)
 # CV
 lme_fit_cv_rs <- lme(
   cv_glucose ~ Age,
@@ -108,17 +89,10 @@ joint_fit_cv <- jm(
   lme_fit_cv_rs,
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save and clean up memory
-save(
-  lme_fit_cv_rs,
-  joint_fit_cv,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_cv.RData"
-)
-rm(lme_fit_cv_rs, joint_fit_cv)
 # TAR
 lme_fit_tar_rs = mixed_model(
   perc_time_over_140 ~ Age,
@@ -131,17 +105,10 @@ joint_fit_tar <- jm(
   lme_fit_tar_rs,
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save and clean up memory
-save(
-  lme_fit_tar_rs,
-  joint_fit_tar,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_tar.RData"
-)
-rm(lme_fit_tar_rs, joint_fit_tar)
 # HbA1c
 lme_fit_hba1c_rs <- lme(
   hba1c ~ Age,
@@ -153,13 +120,23 @@ joint_fit_hba1c <- jm(
   lme_fit_hba1c_rs,
   time_var = "Age",
   id_var = "ID",
-  n_iter = 1e6,
+  n_iter = 5e5,
   n_burnin = 1e3,
   control = list(n_chains = 4, cores = 1)
 )
-# Save
+# Save everything
 save(
+  cox_fit,
+  lme_fit_mean_rs,
+  joint_fit_mean,
+  lme_fit_sd_rs,
+  joint_fit_sd,
+  joint_fit_mean_sd,
+  lme_fit_cv_rs,
+  joint_fit_cv,
+  lme_fit_tar_rs,
+  joint_fit_tar,
   lme_fit_hba1c_rs,
   joint_fit_hba1c,
-  file = "./Data_Clean/Joint Model Results/All/joint_fit_hba1c.RData"
+  file = "./Data_Clean/joint_model_results_all.RData"
 )
